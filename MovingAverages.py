097f1c3a62c91c1f26dc2f276a01f388b27de048
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2017-03-09 00:06:14 -0800

    Update MovingAverages.py

--- MovingAverages.py
+++ MovingAverages.py
@@ -4,8 +4,11 @@ Spyder Editor
 
 from pandas_datareader import data
 import numpy as np
-s = data.DataReader('^GSPC', 'yahoo', start='1/1/2010', end='01/01/2050')
+ticker = '^GSPC'
+s = data.DataReader(ticker, 'yahoo', start='1/1/2010', end='01/01/2050')
+a = 50 #moving average window
+b = 151 #moving average window
 s['LogReturns'] = np.log(s['Adj Close']/s['Adj Close'].shift(1))
-s['Small'] = s['Adj Close'].rolling(window=56, center=False).mean()
-s['Large'] = s['Adj Close'].rolling(window=151, center=False).mean()
+s['Small'] = s['Adj Close'].rolling(window=a, center=False).mean()
+s['Large'] = s['Adj Close'].rolling(window=b, center=False).mean()
 s[['Small', 'Large', 'Close']].plot(grid=True, figsize=(8, 5))
