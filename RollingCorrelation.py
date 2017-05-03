9f54aad45b8fcf91b00577dfdb3c3a3996b2d05e
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2017-05-02 21:25:20 -0700

    Update RollingCorrelation.py

--- RollingCorrelation.py
+++ RollingCorrelation.py
@@ -1,6 +1,7 @@
 """
 Spyder Editor
 """
+#This is simply for two instruments 
 from pandas_datareader import data
 import pandas as pd
 import numpy as np
@@ -12,7 +13,6 @@ s2 = data.DataReader(ticker2, 'yahoo', start='10/1/2015', end='01/01/2050')
 s1['LogRet'] = np.log(s1['Adj Close']/s1['Adj Close'].shift(1))
 s2['LogRet'] = np.log(s2['Adj Close']/s2['Adj Close'].shift(1))
 s3 = pd.DataFrame()
-#s3['Correlation'] = pd.rolling_corr(s1['LogRet'],s2['LogRet'], window)
 s3['Correlation'] = pd.rolling_corr(s1['Adj Close'],s2['Adj Close'], window)
 s3 = s3[window:]
 s3['Correlation'].plot(grid = True, figsize = (8,3))
