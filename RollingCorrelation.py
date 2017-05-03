2214f54b5e472b16726c952fc1cede177afe1b0b
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2017-05-02 22:15:39 -0700

    Update RollingCorrelation.py

--- RollingCorrelation.py
+++ RollingCorrelation.py
@@ -13,6 +13,7 @@ s2 = data.DataReader(ticker2, 'yahoo', start='10/1/2015', end='01/01/2050')
 s1['LogRet'] = np.log(s1['Adj Close']/s1['Adj Close'].shift(1))
 s2['LogRet'] = np.log(s2['Adj Close']/s2['Adj Close'].shift(1))
 s3 = pd.DataFrame()
-s3['Correlation'] = pd.rolling_corr(s1['Adj Close'],s2['Adj Close'], window)
+s3['Correlation'] = pd.rolling_corr(s1['Adj Close'],s2['Adj Close'], window) #Correlation of price, pick this
+#s3['Correlation'] = pd.rolling_corr(s1['LogRet'],s2['LogRet'], window) #Correlation of returns, or pick this
 s3 = s3[window:]
 s3['Correlation'].plot(grid = True, figsize = (8,3))
