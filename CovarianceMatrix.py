b88d24818dcc983e9ae3b16cf4bda16a08e9202c
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2017-05-02 23:13:55 -0700

    Add files via upload

---
+++ CovarianceMatrix.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  2 22:21:03 2017
+
+@author: AmatVictoriaCuramIII
+"""
+
+from pandas_datareader import data
+import pandas as pd
+import numpy as np
+tickers = ['^RUT','GLD','SOYB','JO','TLT']
+returns = pd.DataFrame()
+for s in tickers:
+    s = data.DataReader(s, 'yahoo', start='10/1/2015', end='01/01/2050')
+    s['LogRet'] = np.log(s['Adj Close']/s['Adj Close'].shift(1))
+    returns = pd.concat([returns,s['LogRet']],axis = 1)
+returns.columns = tickers
+returns = returns.fillna(0)
+matrix = returns.cov()
+print(matrix)
