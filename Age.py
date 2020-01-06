ce703270afb40293ac6e6f357181a3c3571186a6
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-01-05 22:24:51 -0800

    Update Age.py

--- Age.py
+++ Age.py
@@ -4,6 +4,10 @@ Created on Tue Feb 28 13:03:32 2017
 
 @author: AmatVictoriaCuramIII
 """
+
+#pandas_datareader is deprecated, use YahooGrabber
+#This is a time series length calculator
+
 from pandas_datareader import data
 def Age(s):
     s = data.DataReader(s, 'yahoo', start='1/1/1900', end='01/01/2050')
