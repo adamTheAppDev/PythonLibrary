d036429c3ee4faa3d729a358ecf78188123c2fd9
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-01-06 20:41:13 -0800

    Update BallerBands.py

--- BallerBands.py
+++ BallerBands.py
@@ -2,6 +2,9 @@
 Spyder Editor
 """
 
+#pandas_datareader is deprecated, use YahooGrabber
+#These are Bollinger Bands, a technical analysis tool
+
 from pandas_datareader import data
 ticker = '^GSPC'
 s = data.DataReader(ticker, 'yahoo', start='1/1/2015', end='01/01/2050')
@@ -12,4 +15,4 @@ s['UpperBand'] = s['nDaySMA'] + (s['nDaySTD'] * 2)
 s['LowerBand'] = s['nDaySMA'] - (s['nDaySTD'] * 2)
 s = s[window:]
 s[['Adj Close', 'nDaySMA', 'UpperBand', 'LowerBand']].plot(grid=True,
-                                                             figsize=(8, 5))
+                                                           figsize=(8, 5))
