34ed40c944183743af490382f448483c10e7cf99
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-01-09 21:54:10 -0800

    Update DualSMAstrategy.py

--- DualSMAstrategy.py
+++ DualSMAstrategy.py
@@ -4,6 +4,9 @@ Created on Wed Aug 30 19:07:37 2017
 
 @author: AmatVictoriaCuramIII
 """
+
+#This is a brute force optimization single asset strategy tester
+
 import numpy as np
 import random as rand
 import pandas as pd
