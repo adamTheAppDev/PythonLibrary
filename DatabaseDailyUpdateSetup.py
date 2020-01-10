ba7bf87a5ab617c2c0b01a69a4ecacd05f3fefda
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-01-09 21:36:08 -0800

    Update DatabaseDailyUpdateSetup.py

--- DatabaseDailyUpdateSetup.py
+++ DatabaseDailyUpdateSetup.py
@@ -4,6 +4,9 @@ Created on Wed May 24 20:13:30 2017
 
 @author: AmatVictoriaCuramIII
 """
+
+#Prep for batch file usage
+
 from distutils.core import setup
 import py2exe
 setup(console=['DatabaseDailyUpdate.py'])
