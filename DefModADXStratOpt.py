d1ef19df4425985fff7d009194285b514e27ed1c
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-01-09 18:36:41 -0800

    Update DefModADXStratOpt.py

--- DefModADXStratOpt.py
+++ DefModADXStratOpt.py
@@ -5,6 +5,8 @@ Created on Sun Apr  9 16:36:25 2017
 @author: AmatVictoriaCuramIII
 """
 
+#This is a brute force optimization tool that is part of a kth fold optimization tool
+
 import pandas as pd
 from pandas_datareader import data
 import numpy as np
