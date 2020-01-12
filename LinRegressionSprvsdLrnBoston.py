cb9210bb9d483073df6a5f6e0b8072b7f72a6746
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-01-12 12:30:54 -0800

    Update LinRegressionSprvsdLrnBoston.py

--- LinRegressionSprvsdLrnBoston.py
+++ LinRegressionSprvsdLrnBoston.py
@@ -8,7 +8,6 @@ Created on Sun Oct  6 19:04:59 2019
 #This is from the https://github.com/amueller and Guido Python ML book
 #Supervised Learning Binary Classification Wisconsin Data Linear Regression
 
-
 #import sys
 #import sklearn
 #import numpy as np
@@ -29,7 +28,6 @@ X, y = mglearn.datasets.load_extended_boston()
 print("Data shape: {}".format(X.shape))
 print("--------------------------------")
 
-#
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 #fit linear model
 lr = LinearRegression().fit(X_train, y_train)
