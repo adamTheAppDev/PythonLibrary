8960b56f0647c5f65105e8debab6aca0b0d35043
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-01-12 16:49:48 -0800

    Update ListPairs.py

--- ListPairs.py
+++ ListPairs.py
@@ -2,10 +2,13 @@
 """
 Created on Sat Nov  4 00:37:10 2017
 
-@author: Adam Reinhold Von Fisher - adamrvfisher@gmail.com 
-linkedin.com/in/adamrvfisher - github.com/adamrvfisher/TechnicalAnalysisLibrary
+@author: Adam Reinhold Von Fisher 
+linkedin.com/in/adamrvfisher 
 """
-#pairing a list into a comprehensive list of pairs.
+
+#This program takes a list and pairs all combinations into a comprehensive list of pairs.
+#It plugs in to the speed dater
+
 def ListPairs(tickers):
     import itertools as it
     sublist = it.combinations(tickers, 2)
