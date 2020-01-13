ea759ccb66d771ac5f73e6f3e1bc6914c76bf162
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-01-12 21:00:48 -0800

    Update MultiCSVFetchYahoo.py

--- MultiCSVFetchYahoo.py
+++ MultiCSVFetchYahoo.py
@@ -5,6 +5,8 @@ Created on Fri May 19 16:53:41 2017
 @author: AmatVictoriaCuramIII
 """
 
+#This is an HTML scraper and formatting tool for database construction
+
 from pandas import read_csv 
 import requests
 import pandas as pd
