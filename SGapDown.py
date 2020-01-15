fdf3fc891d66d0ed2f3b15a908630578eff401c4
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-01-15 06:46:25 -0800

    Update SGapDown.py

--- SGapDown.py
+++ SGapDown.py
@@ -2,6 +2,9 @@
 """
 Spyder Editor
 """
+
+#This is a summary statistic + database query tool
+
 def SGapDown(s):
     s['GapDown'] = (s['Low'].shift(1) - s['High']) / s['Adj Close'].shift(1)
     s['GapDown'] = s['GapDown'][s['GapDown'] > 0]
