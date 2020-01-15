af9891af6e53c1e83a92bd488d8f60668282ed9f
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-01-15 06:42:48 -0800

    Update SAverageRollingVolume.py

--- SAverageRollingVolume.py
+++ SAverageRollingVolume.py
@@ -4,6 +4,9 @@ Created on Tue Feb 28 13:31:53 2017
 
 @author: AmatVictoriaCuramIII
 """
+
+#This is a summary statistic + database query tool
+
 def SAverageRollingVolume(s):
     s['AverageRollingVolume'] = s['Volume'].rolling(center=False, 
                                                         window=252).mean()
