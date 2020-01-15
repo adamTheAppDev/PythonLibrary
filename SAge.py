aae60ea654f90b7d7410d1898ffd38ef196a37b6
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-01-15 06:43:14 -0800

    Update SAge.py

--- SAge.py
+++ SAge.py
@@ -5,7 +5,7 @@ Created on Tue Feb 28 13:03:32 2017
 @author: AmatVictoriaCuramIII
 """
 
-#This is a database query tool
+#This is a summary statistic + database query tool
 
 def SAge(s):
     return int(len(s['Adj Close']))
