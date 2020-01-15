d1bdd3e6108dc41bcebc5a5eb46e9c6f877f9665
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-01-15 08:24:27 -0800

    Update WindowedView.py

--- WindowedView.py
+++ WindowedView.py
@@ -4,10 +4,14 @@ Created on Sun Sep  3 22:41:25 2017
 
 @author: AmatVictoriaCuramIII
 """
+
+#This is a windowed view tool - a formatting tool - stolen from the depths of the internet
+#I probably rewrote it into another program to fit my data.
+
 import numpy as np
 from numpy.lib.stride_tricks import as_strided
 import pandas as pd
-#Stolen windowed view
+
 def WindowedView(x, window_size):
     """Creat a 2d windowed view of a 1d array.
 
