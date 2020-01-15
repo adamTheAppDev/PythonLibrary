5af50b35e1f2101ca83e5f7f266ce4c1e27f6c01
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-01-15 06:43:53 -0800

    Update SCoeffVar.py

--- SCoeffVar.py
+++ SCoeffVar.py
@@ -4,6 +4,9 @@ Created on Tue Feb 28 13:28:23 2017
 
 @author: AmatVictoriaCuramIII
 """
+
+#This is a summary statistic + database query tool
+
 import numpy as np
 def SCoeffVar(s):
     s['LogRet'] = np.log(s['Adj Close']/s['Adj Close'].shift(1))
