e25052e11d538b3d0a4413da52e3191ae167fb60
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-01-15 06:57:20 -0800

    Update STrend.py

--- STrend.py
+++ STrend.py
@@ -4,6 +4,9 @@ Created on Tue Feb 28 13:26:20 2017
 
 @author: AmatVictoriaCuramIII
 """
+
+#This is a technical analysis tool
+
 def STrend(s):# + denotes bullish, - denotes bearish
     s['42d'] = s['Adj Close'].rolling(window=42, center=False).mean()
     s['252d'] = s['Adj Close'].rolling(window=252, center=False).mean()
