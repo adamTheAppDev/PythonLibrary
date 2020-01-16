f28c2b01b8d089171b1706fee8759bd56e99fb03
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-01-15 21:29:40 -0800

    Update YahooSymbols.py

--- YahooSymbols.py
+++ YahooSymbols.py
@@ -4,6 +4,9 @@ Created on Sat May 20 16:33:06 2017
 
 @author: AmatVictoriaCuramIII
 """
+
+#This is an outdated database query tool
+
 import os 
 import pandas as pd
 index = range(0,len(os.listdir('F:\\Users\\AmatVictoriaCuram\\Database')))
