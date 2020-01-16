6b753ce2e26b3b12015be6468030e2f4d25d3e5a
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-01-15 21:34:06 -0800

    Update multithreadADXStratOptsExecution.py

--- multithreadADXStratOptsExecution.py
+++ multithreadADXStratOptsExecution.py
@@ -5,7 +5,7 @@ Created on Tue Apr 18 14:52:56 2017
 @author: AmatVictoriaCuramIII
 """
 
-#This is part of a multithreading tool to speed up brute force optimization
+#This is part of a multithreading tool to speed up brute force optimization - looks under construction
 
 from multithreadADXStratOpt import multithreadADXStratOpt
 import numba as nb
