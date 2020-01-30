1b1adb438bdc805647bf40784f03f06594e8f440
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-01-30 06:22:03 -0800

    Update ADXGit.py

--- ADXGit.py
+++ ADXGit.py
@@ -4,15 +4,22 @@ Spyder Editor
 #pandas_datareader is deprecated, use YahooGrabber
 #This is a techincal analysis tool
 
+#Import modules
 from pandas_datareader import data
 import numpy as np
+
+#Variable assignment
 ticker = '^GSPC'
-s = data.DataReader(ticker, 'yahoo', start='01/09/2010', end='01/01/2050')
 window = 14
-s['UpMove'] = s['High'] - s['High'].shift(1)
-s['DownMove'] = s['Low'] - s['Low'].shift(1)
+
+#Request data - Use YahooGrabber
+s = data.DataReader(ticker, 'yahoo', start='01/09/2010', end='01/01/2050')
+
+#Return calculations
 s['LogRet'] = np.log(s['Adj Close']/s['Adj Close'].shift(1)) 
 s['LogRet'] = s['LogRet'].fillna(0)
+
+#ATR calculations
 s['Method1'] = s['High'] - s['Low']
 s['Method2'] = abs((s['High'] - s['Adj Close'].shift(1)))
 s['Method3'] = abs((s['Low'] - s['Adj Close'].shift(1)))
@@ -24,12 +31,16 @@ s['AverageTrueRange'] = s['TrueRange'].rolling(window = window,
                                 center=False).sum()
 s['AverageTrueRange'] = ((s['AverageTrueRange'].shift(1)*(window-1
                              ) + s['TrueRange']) / window)
-s['PDM'] = (s['High'] - s['High'].shift(1))
+
+#ADX calculations
+s['PDM'] = (s['High'] - s['High'].shift(1)
 s['MDM'] = (s['Low'].shift(1) - s['Low'])
 s['PDM'] = s['PDM'][s['PDM'] > 0]
 s['MDM'] = s['MDM'][s['MDM'] > 0]
 s['PDM'] = s['PDM'].fillna(0)
 s['MDM'] = s['MDM'].fillna(0)
+            
+#Smoothing by averages
 s['SmoothPDM'] = s['PDM'].rolling(window = window,
                                 center=False).sum()
 s['SmoothPDM'] = ((s['SmoothPDM'].shift(1)*(window-1
@@ -38,6 +49,8 @@ s['SmoothMDM'] = s['MDM'].rolling(window = window,
                                 center=False).sum()
 s['SmoothMDM'] = ((s['SmoothMDM'].shift(1)*(window-1
                              ) + s['MDM']) / window)
+      
+#Normalize for volatility
 s['PDI'] = (100*(s['SmoothPDM']/s['AverageTrueRange']))
 s['MDI'] = (100*(s['SmoothMDM']/s['AverageTrueRange']))
 s['DIdiff'] = abs(s['PDI'] - s['MDI'])
@@ -46,5 +59,8 @@ s['DX'] = (100 * (s['DIdiff']/s['DIsum']))
 s['ADX'] = s['DX'].rolling(window = window, center = False).mean()
 s['ADXmean'] = s['ADX'].mean()
 trim = (window * 2 - 1)
+            
+#Trim for graphing
 s = s[trim:]
+#Graphical display
 s[['PDI','MDI','ADX','ADXmean']].plot(grid=True, figsize=(8,3))
