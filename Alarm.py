b56549258eb13fd7bdc8076f3714f015aea5a6a2
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-02-01 11:36:04 -0800

    Update Alarm.py

--- Alarm.py
+++ Alarm.py
@@ -1,17 +1,22 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jan 22 00:22:53 2018
 
-@author: AmatVictoriaCuramIII
+@author: Adam Reinhold Von Fisher - https://www.linkedin.com/in/adamrvfisher/
+
 """
 
 #This is an alarm clock, turn up the volume!
 
+#Import modules
 import webbrowser
 import time
 
+#Input URL
 url = 'https://www.youtube.com/watch?v=V2R0K7iX83E'
+#Input hours until alarm
 totalhours = 8 
+#Calculate seconds until alarm
 total = totalhours * 60 * 60
 time.sleep(total)
+#Open URL - turn up volume!
 webbrowser.open(url)
