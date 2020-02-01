5a9ad7c314ff81d0897cc68ab2d09fe9847e40dd
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-02-01 11:46:29 -0800

    Update Age.py

--- Age.py
+++ Age.py
@@ -5,15 +5,14 @@
 
 """
 
-#pandas_datareader is deprecated, use YahooGrabber
 #This is a summary statistic tool
 
 #Import modules 
-from pandas_datareader import data # Use YahooGrabber
+from YahooGrabber import YahooGrabber
 
 #Define function
 def Age(s):
-    #Request data - Use YahooGrabber
-    s = data.DataReader(s, 'yahoo', start='1/1/1900', end='01/01/2050')
-    #Output
+    #Request data
+    s = YahooGrabber(s)
+    #Output age
     return len(s['Adj Close'])
