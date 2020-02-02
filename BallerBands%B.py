0a45411f0d84fdbdab918986ce0f2c00e42590eb
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-02-01 21:49:33 -0800

    Update BallerBands%B.py

--- BallerBands%B.py
+++ BallerBands%B.py
@@ -1,18 +1,30 @@
-"""
-Spyder Editor
+# -*- coding: utf-8 -*-
 """
 
-#pandas_datareader is deprecated, use YahooGrabber
+@author: Adam Reinhold Von Fisher - https://www.linkedin.com/in/adamrvfisher/
+
+"""
+
 #This is a technical analysis tool
 
-from pandas_datareader import data
+#import modules
+from YahooGrabber import YahooGrabber
+
+#input ticker
 ticker = '^GSPC'
-s = data.DataReader(ticker, 'yahoo', start='01/01/2017', end='01/01/2050')
+#Request data
+s = YahooGrabber(ticker)
+#Variable assignment
 window = 20
+#Rolling statistics
 s['nDaySMA'] = s['Adj Close'].rolling(window=window, center=False).mean()
 s['nDaySTD'] = s['Adj Close'].rolling(window=window, center=False).std()
+#Define bands
 s['UpperBand'] = s['nDaySMA'] + (s['nDaySTD'] * 2)
 s['LowerBand'] = s['nDaySMA'] - (s['nDaySTD'] * 2)
+#Difference between Adj Close and Lower band normalized by band width
 s['B'] = (s['Adj Close'] - s['LowerBand'])/(s['UpperBand'] - s['LowerBand'])
+#Trim window off for indicator display
 s = s[window:]
+#Graphical display
 s['B'].plot(grid=True, figsize=(8, 3))
