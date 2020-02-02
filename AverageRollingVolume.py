2b7142dfd058ddfe0f35a7ab14c93f3ba7d75595
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-02-01 20:48:08 -0800

    Update AverageRollingVolume.py

--- AverageRollingVolume.py
+++ AverageRollingVolume.py
@@ -5,12 +5,16 @@ Created on Tue Feb 28 13:31:53 2017
 @author: AmatVictoriaCuramIII
 """
 
-#pandas_datareader is deprecated, use YahooGrabber
 #This is a summary statistic tool
 
-from pandas_datareader import data
+#Import modules
+from YahooGrabber import YahooGrabber
+
+#Define function - input string = 'SPY'
 def AverageRollingVolume(s):
-    s = data.DataReader(s, 'yahoo', start='1/1/1900', end='01/01/2050')
+    s = YahooGrabber(s)
+    #Avg rolling volume calculation 
     s['AverageRollingVolume'] = s['Volume'].rolling(center=False, 
                                                         window=252).mean()
+    #Output
     return s['AverageRollingVolume'].tail(1)
