0850b1b156aafe8d88610496e647242a49c19fd0
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-02-04 07:13:44 -0800

    Update BetaWeighter.py

--- BetaWeighter.py
+++ BetaWeighter.py
@@ -1,47 +1,60 @@
-
-"""
-Created on Thu Aug 31 08:55:46 2017
- -*- coding: utf-8 -*-
-@author: AmatVictoriaCuramIII
+# -*- coding: utf-8 -*-
 """
 
-#pandas_datareader is deprecated, use YahooGrabber
+@author: Adam Reinhold Von Fisher - https://www.linkedin.com/in/adamrvfisher/
+
+"""
+
 #This is a portfolio analysis tool, useful for pair trades and hedging
 
+#Import modules
 import numpy as np
-from pandas_datareader import data
 import random as rand
 import pandas as pd
 import time as t
 from DatabaseGrabber import DatabaseGrabber
 from YahooGrabber import YahooGrabber
-empty = []
-dataset = pd.DataFrame()
-asone = pd.DataFrame()
+
+#Assign variables
+Empty = []
+Dataset = pd.DataFrame()
 start = t.time()
+
+#Request data
 Ticker1 = YahooGrabber('^VIX') #we have these deltas
 Ticker2 = YahooGrabber('UVXY') #we compute these deltas
 
-conversionfactor = Ticker2['Adj Close'][-1]/Ticker1['Adj Close'][-1]
-Ticker1delta = 1000
-Ticker1cost = Ticker1['Adj Close'][-1]
-#Ticker1cost = 18.3
-Ticker1notional = Ticker1delta * Ticker1cost
-#positioning
-Ticker1position = .57
-Ticker2position = .43
-totalnotional = abs(Ticker1notional / Ticker1position)
-Ticker2notional = totalnotional - abs(Ticker1notional)
-Ticker2cost = Ticker2['Adj Close'][-1]
-Ticker2delta = np.round((Ticker2notional / Ticker2cost), decimals = 2)
-notionalcheck = str((Ticker2cost * Ticker2delta) / totalnotional)
-print('If you have ' + str(Ticker1delta) + ' deltas in Ticker1, to reach ' +
-            str(Ticker1position*100)  + " / " + str(Ticker2position*100) + " you must get " + str(Ticker2delta) +
+#Spot price ratios
+ConversionFactor = Ticker2['Adj Close'][-1]/Ticker1['Adj Close'][-1]
+
+#Number of deltas/shares in ticker 1
+Ticker1Delta = 1000
+#Spot price ticker 1
+Ticker1Cost = Ticker1['Adj Close'][-1]
+#Total position value
+Ticker1Notional = Ticker1Delta * Ticker1Cost
+#Portfolio allocation
+Ticker1Position = .57
+Ticker2Position = .43
+
+#Desired value of portfolio 
+TotalNotional = abs(Ticker1Notional / Ticker1Position)
+
+#Decuding position in ticker 2
+Ticker2Notional = TotalNotional - abs(Ticker1Notional)
+Ticker2Cost = Ticker2['Adj Close'][-1]
+Ticker2Delta = np.round((Ticker2Notional / Ticker2Cost), decimals = 2)
+
+#Confirming
+NotionalCheck = str((Ticker2Cost * Ticker2Delta) / TotalNotional)
+
+print('If you have ' + str(Ticker1Delta) + ' deltas in Ticker1, to reach ' +
+            str(Ticker1Position*100)  + " / " + str(Ticker2Position*100) + " you must get " + str(Ticker2Delta) +
             ' deltas in Ticker2 to rebalance.')
-notionalvol = np.round((Ticker2position + float(notionalcheck)), decimals = 2)
+NotionalVol = np.round((Ticker2Position + float(NotionalCheck)), decimals = 2)
 print('The price deviation from the model suggests a tracking error of ' +
-        str(((1-notionalvol))) + '%')
+        str(((1-NotionalVol))) + '%')
 print('The total cost of the positions using shares will cost $' + 
-            str(int(totalnotional)) +'.')
-print('Ticker1 - $' + str(abs(Ticker1notional)))
-print('Ticker2 - $' + str(Ticker2notional))
+            str(int(TotalNotional)) +'.')
+print('Ticker1 - $' + str(abs(Ticker1Notional)))
+print('Ticker2 - $' + str(Ticker2Notional))
