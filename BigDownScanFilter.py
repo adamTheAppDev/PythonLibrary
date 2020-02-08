5d0ed48b3b86d7694896962045b86a54b61ab1b7
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-02-08 07:30:40 -0800

    Update BigDownScanFilter.py

--- BigDownScanFilter.py
+++ BigDownScanFilter.py
@@ -5,11 +5,10 @@ Created on Tue Feb 28 23:29:44 2017
 @author: AmatVictoriaCuramIII
 """
 
-#pandas_datareader is deprecated, use YahooGrabber
 #This is a scanning tool with technical analysis modules inside
 #There is a more efficient way to do this process somewhere in the MasterLibrary
 
-#Get modules
+#Get modules to filter
 from SAge import SAge
 from SAverageReturn import SAverageReturn
 from SCoeffVar import SCoeffVar
@@ -19,23 +18,33 @@ from DayOverAverageRollingVolume import DayOverAverageRollingVolume
 from SAverageRollingVolume import SAverageRollingVolume
 from SAdjustedClose import SAdjustedClose
 from pandas import read_csv
-from pandas_datareader import data
-#Import and define info the list 
+from YahooGrabber import YahooGrabber
+
+#Iteration counter
 counter = 1
+
+#Import stocks to filter 
 df = read_csv('companylist.csv', sep = ',')
+#Formatting
 df1 = df.set_index('Symbol')
 symbols = df.Symbol.values
+#Define portfolio of stocks to scan
 port = (symbols)
+#End portfolio of stocks that pass scan
 port1 = []
 print(symbols)
+#For all tickers to be scanned
 for s in port:
     try:
-        q = data.DataReader(s, 'yahoo', start='1/1/1950', end='01/01/2050')        
+        #Data req is faster from local storage than HTML parse
+        q = YahooGrabber(s)
+        #If passes scan
         if sum(SAdjustedClose(q)) > 5 and SAge(q) > 750 and sum(SGapDown(q)
         ) > .01 and sum(SAverageRollingVolume(q)
         ) > 500000 and sum(DayOverAverageRollingVolume(q)
         ) > 2 and sum(SAverageReturn(q)) < -.01 and sum(STrend(q)) < -0.01:
             print(s,  STrend(q), DayOverAverageRollingVolume(q), SAge(q))
+            #Add to final list
             port1.append(s)
     except OSError:
         pass
