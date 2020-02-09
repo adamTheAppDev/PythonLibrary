a1b252e56a14ec861893e1da91628441b5f0eb19
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-02-09 11:42:27 -0800

    Update BollingerBands.py

--- BollingerBands.py
+++ BollingerBands.py
@@ -1,18 +1,32 @@
-"""
-Spyder Editor
+# -*- coding: utf-8 -*-
 """
 
-#pandas_datareader is deprecated, use YahooGrabber
+@author: Adam Reinhold Von Fisher - https://www.linkedin.com/in/adamrvfisher/
+
+"""
+
 #These are Bollinger Bands, a technical analysis tool
 
+#Import modules
+from YahooGrabber import YahooGrabber
 from pandas_datareader import data
+
+#Input ticker
 ticker = '^GSPC'
-s = data.DataReader(ticker, 'yahoo', start='1/1/2015', end='01/01/2050')
+s = YahooGrabber(ticker)
+
+#Variable assignment
 window = 20
+
+#SMA/STD calculation
 s['nDaySMA'] = s['Adj Close'].rolling(window=window, center=False).mean()
 s['nDaySTD'] = s['Adj Close'].rolling(window=window, center=False).std()
+#Band calculation
 s['UpperBand'] = s['nDaySMA'] + (s['nDaySTD'] * 2)
 s['LowerBand'] = s['nDaySMA'] - (s['nDaySTD'] * 2)
+#Trim data
 s = s[window:]
+
+#Graphical display
 s[['Adj Close', 'nDaySMA', 'UpperBand', 'LowerBand']].plot(grid=True,
                                                            figsize=(8, 5))
