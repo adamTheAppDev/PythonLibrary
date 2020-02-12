5018bcdf5c80639163bd2cbe6dd6001c7b2c5651
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-02-12 08:42:27 -0800

    Update CSVConcatenator.py

--- CSVConcatenator.py
+++ CSVConcatenator.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Aug 11 00:35:14 2017
 
-@author: AmatVictoriaCuramIII
+@author: Adam Reinhold Von Fisher - https://www.linkedin.com/in/adamrvfisher/
+
 """
 
 #This is a formatting tool for CSV files
