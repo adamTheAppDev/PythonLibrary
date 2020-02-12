4a900b9db5ca8357e6623372bcc1985944f3da27
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-02-11 20:50:00 -0800

    Update CSVConcatenator.py

--- CSVConcatenator.py
+++ CSVConcatenator.py
@@ -6,20 +6,28 @@ Created on Fri Aug 11 00:35:14 2017
 """
 
 #This is a formatting tool for CSV files
-#Database management tool
+#Directory management tool
 
+#Import modules
 from pandas import read_csv
 import pandas as pd
 import os
+
 #Get list of files in folder
 CSVlist = os.listdir('F:\\Users\\AmatVictoriaCuram\\CSVtoSQL\\')
+
 #Make large dataframe
-DailyDose = pd.DataFrame()
+DF = pd.DataFrame()
+
 #Open and mod each file
 for i in CSVlist:
     temp = read_csv('F:\\Users\\AmatVictoriaCuram\\CSVtoSQL\\' +
                          (i), sep = ',') 
-    DailyDose = pd.concat([DailyDose, temp], axis = 0)
-DailyDose = DailyDose.loc[:,~DailyDose.Ticker.duplicated()]
-DailyDose.to_csv('C:\\Users\\AmatVictoriaCuramIII\\Desktop\\Access\\DailyDose.csv',
+    DF = pd.concat([DF, temp], axis = 0)
+    
+#Remove duplicate columns
+DF = DF.loc[:,~DF.Ticker.duplicated()]
+
+#Write to CSV
+DF.to_csv('C:\\Users\\AmatVictoriaCuramIII\\Desktop\\Access\\DF.csv',
                     index=False)
