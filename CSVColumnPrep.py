22c0e493d75137b30152a414f16eb40cb82815b8
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-02-11 20:46:28 -0800

    Update CSVColumnPrep.py

--- CSVColumnPrep.py
+++ CSVColumnPrep.py
@@ -6,8 +6,9 @@ Created on Thu Aug 10 23:37:21 2017
 """
 
 #This is a formatting tool for database management involving SQL
-
 #TempCSV transfer to CSVtoSQL with Access/SQL COLUMN HEADERS
+
+#import modules
 import pandas as pd
 import time as t
 import webbrowser as web
@@ -17,18 +18,21 @@ import datetime
 import os
 import numpy as np
 from pandas import read_csv 
+
 #Get list of files in folder
 CSVlist = os.listdir('F:\\Users\\AmatVictoriaCuram\\TemporaryCSV\\')
+
 #Make program output destination
 if not os.path.exists('F:\\Users\\AmatVictoriaCuram\\CSVtoSQL'):
     os.makedirs('F:\\Users\\AmatVictoriaCuram\\CSVtoSQL')
+    
 #Open and mod each file
 for i in CSVlist:
     temp = read_csv('F:\\Users\\AmatVictoriaCuram\\TemporaryCSV\\' +
                          (i), sep = ',')
     temp.insert(1,'Ticker', i[:-4])
     temp.rename(columns={'Date':'StkDate'}, inplace=True)
-#save files to new destination
+    
+#Save files to new destination
     temp.to_csv('F:\\Users\\AmatVictoriaCuram\\CSVtoSQL\\' + (i), sep=',', 
                     index = False)    
-
