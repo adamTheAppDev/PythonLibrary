fe1eca19696c0e91f208b13b3031ac6aa89503bc
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-02-12 08:46:44 -0800

    Update CSVtoDFDiv.py

--- CSVtoDFDiv.py
+++ CSVtoDFDiv.py
@@ -1,43 +1,60 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun May 21 11:47:12 2017
 
-@author: AmatVictoriaCuramIII
+@author: Adam Reinhold Von Fisher - https://www.linkedin.com/in/adamrvfisher/
+
 """
+
+#Import modules
 from pandas import read_csv
 import pandas as pd
 import os
 
-#This is a formatting tool for database management
+#This is a formatting tool for directory management
 
+#Find files to iterate through
 CSVfiles = os.listdir('F:\\Users\\AmatVictoriaCuram\\TemporaryDiv')
+#Create iterable
 ranger = range(0,len(CSVfiles))
+#For all files in folder
 for i in ranger:
     try:
+        #Open file
         temp = read_csv('F:\\Users\\AmatVictoriaCuram\\TemporaryDiv\\' +
                          (CSVfiles[i]), sep = ',')
+        #Set index
         temp = temp.set_index('Date')
+        #Format index
         temp.index = pd.to_datetime(temp.index, format = "%Y/%m/%d") 
+        #Delete duplicate columns
         temp = temp.loc[:,~temp.columns.duplicated()]
+        #Delete duplicate rows
         temp = temp[~temp.index.duplicated(keep='first')]
+        #Create folder if it doesn't exist
         if not os.path.exists('F:\\Users\\AmatVictoriaCuram\\Database\\' +
                           CSVfiles[i][:-7]):
             os.makedirs('F:\\Users\\AmatVictoriaCuram\\Database\\' +
                           CSVfiles[i][:-7])
+        #Save to pickle
         pd.to_pickle(temp, 'F:\\Users\\AmatVictoriaCuram\\Database\\' +
                           CSVfiles[i][:-7] + '\\' + CSVfiles[i][:-4])
     except OSError:
         continue
+#For all files
 for i in ranger:
     try:
-        glaze = pd.read_pickle('F:\\Users\\AmatVictoriaCuram\\Database\\' +
+        files = pd.read_pickle('F:\\Users\\AmatVictoriaCuram\\Database\\' +
                          (CSVfiles[i][:-4]))
-        for x in glaze.columns:
-            glaze[x] =  pd.to_numeric(glaze[x], errors='coerce')
-        pd.to_pickle(glaze, 'F:\\Users\\AmatVictoriaCuram\\Database\\' +
+        #For all columns in file
+        for x in files.columns:
+            #Change str to num -- This code can be implemented in above for loop to improve performance
+            files[x] =  pd.to_numeric(files[x], errors='coerce')
+        #Save file to pickle
+        pd.to_pickle(files, 'F:\\Users\\AmatVictoriaCuram\\Database\\' +
                           CSVfiles[i][:-4])
     except OSError:
         continue
+        
 #this is for testing individual CSVs
 #tester = read_csv('F:\\Users\\AmatVictoriaCuram\\TemporaryCSV\\' +
 #                     (df['CSVname'][0]), sep = ',')
