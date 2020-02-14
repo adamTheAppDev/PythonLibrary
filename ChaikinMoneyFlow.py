9b49c3d6ea6e578a3871ed24cb8870a940e57921
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-02-13 21:41:28 -0800

    Update ChaikinMoneyFlow.py

--- ChaikinMoneyFlow.py
+++ ChaikinMoneyFlow.py
@@ -1,21 +1,39 @@
-"""
-Spyder Editor
+# -*- coding: utf-8 -*-
 """
 
-#pandas_datareader is deprecated, use YahooGrabber
+@author: Adam Reinhold Von Fisher - https://www.linkedin.com/in/adamrvfisher/
+
+"""
+
 #This is a technical analysis tool
 
+#Import modules
+from YahooGrabber import YahooGrabber
 import numpy as np
 from pandas_datareader import data
+
+#Variable assignment
 ticker = '^GSPC'
 window = 20
-s = data.DataReader(ticker, 'yahoo', start='11/01/2016', end='01/01/2050')
+
+#Request data
+s = YahooGrabber(ticker)
+
+#Calculate returns
 s['LogRet'] = np.log(s['Adj Close']/s['Adj Close'].shift(1)) 
 s['LogRet'] = s['LogRet'].fillna(0)
+
+#Money flow multiplier calculation
 s['MFMultiplier'] = (((s['Adj Close'] - s['Low']) - (s['High'] - s['Adj Close']))
                     / (s['High'] - s['Low']))
 s['MFVolume'] = (s['Volume'] * s['MFMultiplier'])
+
+#Horizontal baseline
 s['ZeroLine'] = 0
+
+#Chaikin money flow calculation
 s['CMF'] = s['MFVolume'].rolling(center=False, window=window).sum(
         )/s['Volume'].rolling(center=False, window=window).sum()
+
+#Graphical display
 s[['CMF','ZeroLine']][window:].plot(grid=True, figsize=(8,3))
