427842ed763ac5eb074cf439f95e270133cccc12
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-02-15 07:32:10 -0800

    Update CommodityChannelIndex.py

--- CommodityChannelIndex.py
+++ CommodityChannelIndex.py
@@ -1,19 +1,40 @@
+# -*- coding: utf-8 -*-
 """
-Spyder Editor
+
+@author: Adam Reinhold Von Fisher - https://www.linkedin.com/in/adamrvfisher/
+
 """
 
 #This is a technical analysis tool
 
-from DatabaseGrabber import DatabaseGrabber
+#Import modules
+from YahooGrabber import YahooGrabber
+
+#Assign variables
 ticker = 'IWM'
 constant = .02
 SMAwindow = 20
-s = DatabaseGrabber(ticker)
+
+s = YahooGrabber(ticker)
+
+#Calculate typical price
 s['TP'] = (s['High'] + s['Low'] + s['Adj Close']) / 3
+
+#Calculate typical price simple moving average
 s['TPSMA'] = s['TP'].rolling(center=False, window = SMAwindow).mean()
+
+#Calculate average STDev
 s['MeanDeviation'] = s['TP'].rolling(center=False, window = SMAwindow).std()
+
+#Calculate commodity channel index
 s['CCI'] = ((s['TP'] - s['TPSMA'])/(constant*s['MeanDeviation']))
+
+#Top and bottom horizontal lines
 s['Top'] = 100
 s['Bottom'] = -100
+
+#Trim data for indicator graph
 s = s[SMAwindow:]
+
+#Display
 s[['CCI','Top','Bottom']].plot(grid = True, figsize = (8,3))
