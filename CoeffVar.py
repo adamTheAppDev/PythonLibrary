21134b07fbe215b2c9e4c72be490e662ee615d1d
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-02-15 07:25:55 -0800

    Update CoeffVar.py

--- CoeffVar.py
+++ CoeffVar.py
@@ -1,19 +1,28 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Feb 28 13:28:23 2017
 
-@author: AmatVictoriaCuramIII
+@author: Adam Reinhold Von Fisher - https://www.linkedin.com/in/adamrvfisher/
+
 """
 
-#pandas_datareader is deprecated, use YahooGrabber
+
 #This is a summary statistic calculator
 
+#Import modules
 import numpy as np
-from pandas_datareader import data
+from YahooGrabber import YahooGrabber
+
+#Define function, param
 def CoeffVar(s):
-    s = data.DataReader(s, 'yahoo', start='1/1/1900', end='01/01/2050')
+    #Request data
+    s = YahooGrabber(s)
+    #Calculate log returns
     s['LogRet'] = np.log(s['Adj Close']/s['Adj Close'].shift(1))
+    #Calculate average return
     s['Mean'] = np.mean(s['LogRet'])*252
+    #Calculate STDev returns
     s['SD'] = np.std(s['LogRet'])*np.sqrt(252)
+    #Calculate Coefficient of variation
     s['CoeffVar'] = s['SD']/s['Mean']
+    #Output
     return s['CoeffVar'].tail(1)
