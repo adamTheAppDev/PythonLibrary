d00e91deb58a7faff712a9448cdd42d9c842113f
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-02-19 20:00:56 -0800

    Update DatabaseCovarianceMatrix.py

--- DatabaseCovarianceMatrix.py
+++ DatabaseCovarianceMatrix.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jun 14 14:34:43 2017
 
-@author: AmatVictoriaCuramIII
+@author: Adam Reinhold Von Fisher - https://www.linkedin.com/in/adamrvfisher/
+
 """
 
 #This is a summary statistic tool for portfolio analysis
-#really gnarly covariance matrix
+#Covariance matrix
 
 import pandas as pd
 import os
 
-logret = pd.read_pickle('F:\\Users\\AmatVictoriaCuram\\Database\\UniverseLogRet\\UniverseLogRet')
+logret = pd.read_pickle('F:\\Users\\Username\\DirectoryLocation\\UniverseLogRet\\UniverseLogRet')
 logret = logret[-60:]
 
 square = logret.cov()
 
-if not os.path.exists('F:\\Users\\AmatVictoriaCuram\\Database\\UniverseCovariance'):
-    os.makedirs('F:\\Users\\AmatVictoriaCuram\\Database\\UniverseCovariance')
+if not os.path.exists('F:\\Users\\Username\\DirectoryLocation\\UniverseCovariance'):
+    os.makedirs('F:\\Users\\Username\\DirectoryLocation\\UniverseCovariance')
     
-pd.to_pickle(square, 'F:\\Users\\AmatVictoriaCuram\\Database\\UniverseCovariance\\Universe12wkCovariance')
+pd.to_pickle(square, 'F:\\Users\\Username\\DirectoryLocation\\UniverseCovariance\\Universe12wkCovariance')
