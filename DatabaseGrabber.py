bf4ad7f9dc11ded4339004a04d26e250afbf241a
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-02-26 21:52:25 -0800

    Update DatabaseGrabber.py

--- DatabaseGrabber.py
+++ DatabaseGrabber.py
@@ -5,14 +5,24 @@
 
 """
 
-#This is a database management/query tool 
+#This is a directory management/query tool 
 
+#Define function 
 def DatabaseGrabber(ticker):    
+    #Import modules
     import pandas as pd
+    
+    #Assign dataframe by reading pickle 
     dataframe = pd.read_pickle('F:\\Users\\UserName\\DirectoryLocation\\' + 
                   ticker + '\\' + ticker)
+    
+    #For all columns in dataframe
     for i in dataframe.columns:
+        #Make numeric datatype
         dataframe[i] =  pd.to_numeric(dataframe[i], errors='coerce')
+    #Erase duplicate columns
     dataframe = dataframe.loc[:,~dataframe.columns.duplicated()]
+    #Erase duplicate rows
     dataframe = dataframe[~dataframe.index.duplicated(keep='first')]
+    #Output dataframe
     return dataframe
