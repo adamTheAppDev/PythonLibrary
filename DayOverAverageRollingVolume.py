d882a23988575e116640bb5594cb57ca113bf049
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-02-29 12:00:24 -0800

    Update DayOverAverageRollingVolume.py

--- DayOverAverageRollingVolume.py
+++ DayOverAverageRollingVolume.py
@@ -7,9 +7,14 @@
 
 #This is a summary statistic tool
 
+#Define function
 def DayOverAverageRollingVolume(s):
+    #Variable assignment
     window = 60
+    #Rolling volume calculation
     s['AverageRollingVolume'] = s['Volume'].rolling(center=False, 
                                                         window=window).mean()
+    #Relative volume calculation
     s['DayOverARV'] = s['Volume']/s['AverageRollingVolume'] 
+    #Output summary statistic
     return s['DayOverARV'].tail(1)
