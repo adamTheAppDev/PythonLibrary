61b171a16f78650e0fb56224ce81ec88c0811259
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-03-27 19:03:47 -0700

    Update ERatioSingleIssueROC.py

--- ERatioSingleIssueROC.py
+++ ERatioSingleIssueROC.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Feb 23 18:13:31 2019
 
-@author: AmatVictoriaCuramIII
+@author: Adam Reinhold Von Fisher - https://www.linkedin.com/in/adamrvfisher/
+
 """
 
 #This is an Edge Ratio calculator for single issue
 #May be deprecated see ERatioSingleIssueDonchianTrendIII.py
 #N Period Edge Ratio Computation
 
-#Import-ant
+#Import modules
 from YahooGrabber import YahooGrabber
 import numpy as np
 import time as t
@@ -18,19 +18,18 @@ import matplotlib.pyplot as plt
 from matplotlib.finance import candlestick_ohlc
 import matplotlib.dates as mdates
  
-#Let's go
+#Start timer
 start = t.time()
 
-#Empty structures
+#Empty data structures
 tempdf = pd.DataFrame()
 edgelist = []
 
 #Variable assignment
-
 #Issue selection
 ticker = 'UVXY'
 
-#Data import
+#Data request
 Asset = YahooGrabber(ticker)
 
 #Params
@@ -108,93 +107,85 @@ tradedates['MAEpoints'] = 0
 for z in iterations:
     #For all dates in tradedates    
     for i in tradedates.RangeIndex:
+        #Empty dataframe
         tempdf = pd.DataFrame()
+        #Add prices to dataframe
         tempdf['Close'] = Asset['Close'].loc[Asset.index[i:i+z]] 
         tempdf['High'] = Asset['High'].loc[Asset.index[i:i+z]] 
         tempdf['Low'] = Asset['Low'].loc[Asset.index[i:i+z]] 
+        #Display dataframe
         print(tempdf)
+        #If long trade
         if tradedates['Regime'].loc[tradedates['RangeIndex'] == i][0] == 1:
+            #MFE non-normalized
             maxup = max(tempdf['High'] - tempdf['Close'][0])
+            #MAE non-normalized
             maxdown = max(tempdf['Close'][0] - tempdf['Low']) 
+            #Assign to dataframe
             tradedates['MFEpoints'].loc[tradedates['RangeIndex'] == i] = maxup
             tradedates['MAEpoints'].loc[tradedates['RangeIndex'] == i] = maxdown
-
+        #If short trade
         if tradedates['Regime'].loc[tradedates['RangeIndex'] == i][0] == -1:
+            #MFE non-normalized
             maxup = max(tempdf['Close'][0] - tempdf['Low']) 
+            #MAE non-normalized
             maxdown = max(tempdf['High'] - tempdf['Close'][0]) 
+            #Assign to dataframe
             tradedates['MFEpoints'].loc[tradedates['RangeIndex'] == i] = maxdown
             tradedates['MAEpoints'].loc[tradedates['RangeIndex'] == i] = maxup
     
-        
+    #Adjust for volatility - ATR
     tradedates['VolAdjMFE'] = tradedates['MFEpoints']/tradedates['AverageTrueRangePoints']
     tradedates['VolAdjMAE'] = tradedates['MAEpoints']/tradedates['AverageTrueRangePoints']
-    
+    #Add all values
     sumMFE = sum(tradedates['VolAdjMFE'])
     sumMAE = sum(tradedates['VolAdjMAE'])
-    
+    #Divide by number of signals
     AvgVolAdjMFE = sumMFE/numsignals
     AvgVolAdjMAE = sumMAE/numsignals 
-    
+    #Edge ratio for specific period 
     edgeratio = AvgVolAdjMFE/AvgVolAdjMAE
-    
+    #Display metric
     print('The ', z, ' day edge ratio is', edgeratio)
+    #Add metric to list
     edgelist.append(edgeratio)
+
+#Apply directional methodology to returns
 Asset['Strategy'] = (Asset['Regime']).shift(1)*Asset['LogRet']
 Asset['Strategy'] = Asset['Strategy'].fillna(0)
+#Returns on $1
 Asset['Multiplier'] = Asset['Strategy'].cumsum().apply(np.exp)
+
+#Incorrectly calculated max drawdown metric
 drawdown =  1 - Asset['Multiplier'].div(Asset['Multiplier'].cummax())
 Asset['drawdown'] =  1 - Asset['Multiplier'].div(Asset['Multiplier'].cummax())
-
 MaxDD = max(drawdown)
 
-
+#Performance metric
 dailyreturn = Asset['Strategy'].mean()
-
 dailyvol = Asset['Strategy'].std()
 sharpe =(dailyreturn/dailyvol)
+
+#Graphical display
 #Asset[['LogRet','Strategy']].cumsum().apply(np.exp).plot(grid=True,
 #                                 figsize=(8,5))
-    
 
-              
+#Variable assignment
 Length = len(Asset['LogRet'])
 Range = range(0,Length)
+
+#Incorrectly calculated
 print(MaxDD*100, '% = Max Drawdown')
 
+#Emtpy data structure
 edgeratioframe = pd.DataFrame(index = iterations)
+#Populate with results
 edgeratioframe['EdgeRatio'] = edgelist
-
+#Plot line graph
 edgeratioframe['EdgeRatio'].plot(grid=True, figsize=(8,5))
+#End timer
 end = t.time()
+#Timer stats
 print((end - start), ' seconds later.')
+#Display highest e-ratio
 print('Max eRatio is', max(edgeratioframe['EdgeRatio']))
-
-##Graphics
-##X and Y axis scale figure
-#figure, axe = plt.subplots(figsize = (10,5))
-##Assign axis labels
-#plt.ylabel(ticker + ' Price')
-#plt.xlabel('Date') 
-##Overlay
-##axe.plot(AssetCopy['IndexToNumber'], Asset['RollingMax'], color = 'green', label = 'RollingMax')
-##axe.plot(AssetCopy['IndexToNumber'], Asset['RollingMin'], color = 'red', label = 'RollingMin')
-##axe.plot(Asset['IndexToNumber'], Asset['SMA'], color = 'black', label = 'SMA')
-#
-##Signal triangles..
-#axe.scatter(Asset.loc[Asset['OriginalTrade'] == 1, 'IndexToNumber'].values, 
-#            Asset.loc[Asset['OriginalTrade'] == 1, 'Adj Close'].values, label='skitscat', color='green', s=75, marker="^")
-#axe.scatter(Asset.loc[Asset['OriginalTrade'] == -1, 'IndexToNumber'].values, 
-#            Asset.loc[Asset['OriginalTrade'] == -1, 'Adj Close'].values, label='skitscat', color='red', s=75, marker="v")
-#
-##Plot the DF values with the figure, object
-#candlestick_ohlc(axe, AssetCopy.values, width=.6, colorup='green', colordown='red')
-#axe.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
-#
-##For ATR
-#figure2, axe2 = plt.subplots(figsize = (10,2))
-#plt.ylabel(ticker + ' ATR Percent')
-#plt.xlabel('Date')
-#axe2.plot(AssetCopy['IndexToNumber'], Asset ['AverageTrueRangePercent'], color = 'black', label = '4wkATRPercent')
-##axe2.plot(AssetCopy['IndexToNumber'], AssetCopy['ATRRollingMax'], color = 'green', label = 'ATRRollingMax')
-##axe2.plot(AssetCopy['IndexToNumber'], AssetCopy['ATRRollingMin'], color = 'red', label = 'ATRRollingMin')
-#axe2.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
