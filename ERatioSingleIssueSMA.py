dba977811821ef008ed602f766f9830e3373eb46
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-03-31 19:26:51 -0700

    Update ERatioSingleIssueSMA.py

--- ERatioSingleIssueSMA.py
+++ ERatioSingleIssueSMA.py
@@ -1,15 +1,16 @@
+  
 # -*- coding: utf-8 -*-
 """
-Created on Sat Feb 23 18:13:31 2019
 
-@author: AmatVictoriaCuramIII
+@author: Adam Reinhold Von Fisher - https://www.linkedin.com/in/adamrvfisher/
+
 """
 
 #This is an Edge Ratio calculator for single issue
 #May be deprecated see ERatioSingleIssueDonchianTrendIII.py
 #N Period Edge Ratio Computation
 
-#Import-ant
+#Import modules
 from YahooGrabber import YahooGrabber
 import numpy as np
 import time as t
@@ -18,22 +19,21 @@ import matplotlib.pyplot as plt
 from matplotlib.finance import candlestick_ohlc
 import matplotlib.dates as mdates
  
-#Let's go
+#Start timer
 start = t.time()
 
-#Empty structures
+#Empty data structures
 tempdf = pd.DataFrame()
 edgelist = []
 
 #Variable assignment
-
 #Issue selection
 ticker = 'UVXY'
 
-#Data import
+#Request data
 Asset = YahooGrabber(ticker)
 
-#Params
+#Assign params
 atrwindow = 20
 smawindow = 50
 edgedays = 20
@@ -104,92 +104,80 @@ tradedates['MAEpoints'] = 0
 for z in iterations:
     #For all dates in tradedates    
     for i in tradedates.RangeIndex:
+        #Empty data structure
         tempdf = pd.DataFrame()
+        #Fill with price data
         tempdf['Close'] = Asset['Close'].loc[Asset.index[i:i+z]] 
         tempdf['High'] = Asset['High'].loc[Asset.index[i:i+z]] 
         tempdf['Low'] = Asset['Low'].loc[Asset.index[i:i+z]] 
+        #Print display
         print(tempdf)
+        #For long trades
         if tradedates['OriginalTrade'].loc[tradedates['RangeIndex'] == i][0] == 1:
+            #MFE assignment
             maxup = max(tempdf['High'] - tempdf['Close'][0])
+            #MAE assignment 
             maxdown = max(tempdf['Close'][0] - tempdf['Low']) 
+            #Assign to dataframe
             tradedates['MFEpoints'].loc[tradedates['RangeIndex'] == i] = maxup
             tradedates['MAEpoints'].loc[tradedates['RangeIndex'] == i] = maxdown
-
+        #For short trades
         if tradedates['OriginalTrade'].loc[tradedates['RangeIndex'] == i][0] == -1:
+            #MFE assignment
             maxup = max(tempdf['Close'][0] - tempdf['Low']) 
+            #MAE assignment
             maxdown = max(tempdf['High'] - tempdf['Close'][0]) 
+            #Assign to dataframe
             tradedates['MFEpoints'].loc[tradedates['RangeIndex'] == i] = maxdown
             tradedates['MAEpoints'].loc[tradedates['RangeIndex'] == i] = maxup
     
-        
+    #Adjust for volatility -- ATR normalization    
     tradedates['VolAdjMFE'] = tradedates['MFEpoints']/tradedates['AverageTrueRangePoints']
     tradedates['VolAdjMAE'] = tradedates['MAEpoints']/tradedates['AverageTrueRangePoints']
-    
+    #Add together
     sumMFE = sum(tradedates['VolAdjMFE'])
     sumMAE = sum(tradedates['VolAdjMAE'])
-    
+    #Average by dividing by number of signals
     AvgVolAdjMFE = sumMFE/numsignals
     AvgVolAdjMAE = sumMAE/numsignals 
-    
+    #Calculate edge ratio
     edgeratio = AvgVolAdjMFE/AvgVolAdjMAE
     
+    #Apply directional methodology to returns
     Asset['Strategy'] = (Asset['Regime']).shift(1)*Asset['LogRet']
     Asset['Strategy'] = Asset['Strategy'].fillna(0)
+    #Returns on $1
     Asset['Multiplier'] = Asset['Strategy'].cumsum().apply(np.exp)
+    
+    #Incorrectly calculated drawdown stat - fix
     drawdown =  1 - Asset['Multiplier'].div(Asset['Multiplier'].cummax())
     Asset['drawdown'] =  1 - Asset['Multiplier'].div(Asset['Multiplier'].cummax())
-    
     MaxDD = max(drawdown)
     
-    
+    #Performance metrics
     dailyreturn = Asset['Strategy'].mean()
-    
     dailyvol = Asset['Strategy'].std()
     sharpe =(dailyreturn/dailyvol)
-#    Asset[['LogRet','Strategy']].cumsum().apply(np.exp).plot(grid=True,
-#                                     figsize=(8,5))
     
+    #Print results
     print('The ', z, ' day edge ratio is', edgeratio)
+    #Add metric to list
     edgelist.append(edgeratio)
               
+#Variable assignment
 Length = len(Asset['LogRet'])
 Range = range(0,Length)
+#Incorrectly calculated
 print(MaxDD*100, '% = Max Drawdown')
-
+#Assign data structure
 edgeratioframe = pd.DataFrame(index = iterations)
+#Add data to structure
 edgeratioframe['EdgeRatio'] = edgelist
-
+#Graphical display
 edgeratioframe['EdgeRatio'].plot(grid=True, figsize=(8,5))
+#End timer
 end = t.time()
+#Timer stats
 print((end - start), ' seconds later.')
+#Print highest e-ratio
 print('Max eRatio is', max(edgeratioframe['EdgeRatio']))
-
-##Graphics
-##X and Y axis scale figure
-#figure, axe = plt.subplots(figsize = (10,5))
-##Assign axis labels
-#plt.ylabel(ticker + ' Price')
-#plt.xlabel('Date') 
-##Overlay
-##axe.plot(AssetCopy['IndexToNumber'], AssetCopy['RollingMax'], color = 'green', label = 'RollingMax')
-##axe.plot(AssetCopy['IndexToNumber'], AssetCopy['RollingMin'], color = 'red', label = 'RollingMin')
-#axe.plot(Asset['IndexToNumber'], Asset['SMA'], color = 'black', label = 'SMA')
-#
-##Signal triangles..
-#axe.scatter(Asset.loc[Asset['OriginalTrade'] == 1, 'IndexToNumber'].values, 
-#            Asset.loc[Asset['OriginalTrade'] == 1, 'Adj Close'].values, label='skitscat', color='green', s=75, marker="^")
-#axe.scatter(Asset.loc[Asset['OriginalTrade'] == -1, 'IndexToNumber'].values, 
-#            Asset.loc[Asset['OriginalTrade'] == -1, 'Adj Close'].values, label='skitscat', color='red', s=75, marker="v")
-#
-###Plot the DF values with the figure, object
-#candlestick_ohlc(axe, AssetCopy.values, width=.6, colorup='green', colordown='red')
-#axe.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
-#
-##For ATR
-#figure2, axe2 = plt.subplots(figsize = (10,2))
-#plt.ylabel(ticker + ' ATR Percent')
-#plt.xlabel('Date')
-##axe2.plot(AssetCopy['IndexToNumber'], Asset ['AverageTrueRangePercent'], color = 'black', label = '4wkATRPercent')
-##axe2.plot(AssetCopy['IndexToNumber'], AssetCopy['ATRRollingMax'], color = 'green', label = 'ATRRollingMax')
-##axe2.plot(AssetCopy['IndexToNumber'], AssetCopy['ATRRollingMin'], color = 'red', label = 'ATRRollingMin')
-##axe2.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
