11300c92840a9b744e668e204053fc9e9cd159f5
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-04-02 08:13:18 -0700

    Update Input+While+Function.py

--- Input+While+Function.py
+++ Input+While+Function.py
@@ -1,25 +1,34 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Apr 13 23:39:56 2017
 
-@author: AmatVictoriaCuramIII
+@author: Adam Reinhold Von Fisher - https://www.linkedin.com/in/adamrvfisher/
+
 """
 
 #Input, while loops, functions
 
+#Definte function
 def GetANumber(LowerBound, UpperBound):
+    #Flag
     BadInput = True
+    #Until valid input received
     while BadInput == True:
         print("Please enter a number within the bounds.")
         ChosenNumber = input("")
+
         try:
+            #Convert to float
             number = float(ChosenNumber)
         except ValueError:
             print("You have not entered a number.")
             continue
+        #Input test
         if number >= LowerBound and number <= UpperBound:
+            #Valid input
             return number
         else:
+            #Invalid input
             BadInput = True
         print("Invalid Input")
+#Run function
 GetANumber(0, 100)
