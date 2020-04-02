1a98b8cc5f1ce93286c890fbbb7c4bb96d1aaeca
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-04-01 18:39:45 -0700

    Update ForgeModelClassificationGraph.py

--- ForgeModelClassificationGraph.py
+++ ForgeModelClassificationGraph.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Oct  6 19:04:59 2019
 
-@author: AmatVictoriaCuramIII
+@author: https://github.com/amueller/ 
+
 """
 
 #This is a model from https://github.com/amueller/ 
 #Supervised Learning - Two feature dataset with Binary Classification
 #K Nearest Neighbor
 
+#Import modules
 #import sys
 #import sklearn
 #import numpy as np
@@ -19,7 +20,8 @@ import matplotlib.pyplot as plt
 #from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-import mglearn #this is built in code.. USE SCIKIT for real
+import mglearn #this is built in code.. USE SCIKIT for real applications
+
 #generate dataset of X and y, X data are 2 features, y data is binary classification
 X, y = mglearn.datasets.make_forge()
 #scatter plot of 2 features
@@ -43,21 +45,20 @@ print("Test set predictions: {}".format(clf.predict(X_test)))
 #print accuracy = clf.score(X_test, y_test)
 print("Test set accuracy: {:.2f}".format(clf.score(X_test, y_test)))
 
-#define figure and axis + num subplots
+#Define figure and axis + num subplots
 fig, axes = plt.subplots(1, 3, figsize=(10, 3))
-#for neighbors 1 3 9 make subplots
+#For neighbors 1 3 9 make subplots
 for n_neighbors, ax in zip([1, 3, 9], axes):
-    #fit method returns the object self, so we can instantiate
+    #Fit method returns the object self, so we can instantiate
     #and fit in one line
     clf = KNeighborsClassifier(n_neighbors=n_neighbors).fit(X, y)
 
-    #mg built in graphs...    
+    #MG built in graphs...    
     mglearn.plots.plot_2d_separator(clf, X, fill=True, eps=0.5, ax=ax, alpha=.4)
     mglearn.discrete_scatter(X[:, 0], X[:, 1], y, ax=ax)
-    #titles and labels
+    #Titles and labels
     ax.set_title("{} neighbor(s)".format(n_neighbors))
     ax.set_xlabel("feature 0")
     ax.set_ylabel("feature 1")
-#what subplot area displays legend
+#Define what subplot area displays legend
 axes[0].legend(loc=3)
-
