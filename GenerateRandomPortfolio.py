5d7cff69ce40fa10f6fc44037ffda0a4921fc2c9
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-04-01 18:51:28 -0700

    Update GenerateRandomPortfolio.py

--- GenerateRandomPortfolio.py
+++ GenerateRandomPortfolio.py
@@ -1,28 +1,35 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jun 16 00:57:19 2017
 
-@author: AmatVictoriaCuramIII
+@author: Adam Reinhold Von Fisher - https://www.linkedin.com/in/adamrvfisher/
+
 """
 
 #This is a portfolio analysis tool, must have all tickers log returns in a table to run 
-
 #Generate Random Portfolio
+
+#Define function
 def GenerateRandomPortfolio(logret):
-    import pandas as pd
-    
-    logret = pd.read_pickle('F:\\Users\\AmatVictoriaCuram\\Database\\MiniUniverseLogRet\\MiniUniverseLogRet')
-    logret = logret[-60:]
-    logret = logret.fillna(0)
+    #Import modules
     import numpy as np
     from RandomWeight import RandomWeight
-    p = np.asmatrix(np.mean(logret, axis = 0)) #expected return
-    w = np.asmatrix(RandomWeight(logret.shape[1])) #weights
-    C = np.asmatrix(np.cov(logret,rowvar=False)) #covar matrix
-    
+    import pandas as pd
+    #This reads in a matrix of log returns from different issues - returns in columns by issue
+    logret = pd.read_pickle('F:\\Users\\AmatVictoriaCuram\\Database\\MiniUniverseLogRet\\MiniUniverseLogRet')
+    #Trim time series
+    logret = logret[-60:]
+    logret = logret.fillna(0)
+    #Expected return matrix
+    p = np.asmatrix(np.mean(logret, axis = 0))
+    #Weights matrix
+    w = np.asmatrix(RandomWeight(logret.shape[1]))
+    #Covariance matrix
+    C = np.asmatrix(np.cov(logret,rowvar=False)) 
+    #Weights times returns.transpose
     mu = w * p.T
+    #StdDev 
     sigma = np.sqrt(w * C * w.T)
-    
+    #StdDev over two
     if sigma > 2:
         return GenerateRandomPortfolio(logret)
     return mu, sigma, w
