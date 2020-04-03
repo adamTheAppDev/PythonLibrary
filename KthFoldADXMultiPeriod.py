e656805a3193848c9c80de8d3b1823e54b0e8533
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-04-03 08:12:58 -0700

    Update KthFoldADXMultiPeriod.py

--- KthFoldADXMultiPeriod.py
+++ KthFoldADXMultiPeriod.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Apr  3 16:08:47 2017
+
+@author: Adam Reinhold Von Fisher - https://www.linkedin.com/in/adamrvfisher/
 
 """
 #pandas_datareader is deprecated, use YahooGrabber
 #This is part of a k-th fold optimization tool
 
+#Import modules
 #import numpy as np
 #from pandas_datareader import data
 import pandas as pd
@@ -14,27 +16,34 @@ import numpy as np
 from pandas_datareader import data
 from DefModADXStratOpt import DefModADXStratOpt
 from ModADXAggMaker import ModADXAggMaker
-#Get timeseries
+
+#Assign ticker
 ticker = 'TLT'
+#Time series splits
 firsttime = '07/01/2002'
 secondtime = '07/01/2007'
 thirdtime = '07/01/2012'
 fourthtime = '01/01/2015'
 lasttime = '01/01/2050'
+#Number of iterations
 multiplier = 400
 ranger1 = range(0,multiplier)
 iterations = 5000
-ranger2 = range(0,iterations) #Pass ranger2
+ranger2 = range(0,iterations)
+#Empty data structures
 empty = []
 counter = 0
 DS1W = pd.DataFrame()
 DS2W = pd.DataFrame()
 DS3W = pd.DataFrame()
 DS4W = pd.DataFrame()
-#number of iterations and sharpe threshold are defined in DefNormChaikinStratOpt
-#this will correspond to DS1W
+
+#DS1W
+#Start timer
 start1 = t.time()
+#Request data
 s = data.DataReader(ticker, 'yahoo', start=firsttime, end=secondtime)
+#ADX calculation
 s['UpMove'] = s['High'] - s['High'].shift(1)
 s['DownMove'] = s['Low'] - s['Low'].shift(1)
 s['LogRet'] = np.log(s['Adj Close']/s['Adj Close'].shift(1)) 
@@ -52,18 +61,26 @@ s['PDM'] = s['PDM'][s['PDM'] > 0]
 s['MDM'] = s['MDM'][s['MDM'] > 0]
 s['PDM'] = s['PDM'].fillna(0)
 s['MDM'] = s['MDM'].fillna(0)
-#the winners from each time period are in the sets below
+#For number of iterations
 for r in ranger1:
+    #Iteration tracking
     print(counter)
     counter = counter + 1
+    #Get random params and generated metrics
     holder = DefModADXStratOpt(ranger2, s)
+    #Add to dataframe
     DS1W = pd.concat([DS1W, holder], axis = 1)
+#End timer
 end1 = t.time()
+#Timer stats
 print('Dataset 1 is optimized, it took',end1-start1,'seconds') #run time in seconds
-#this will correspond to DS2W
+#DS2W
 counter = 0
+#Start timer
 start2 = t.time()
+#Request data
 s = data.DataReader(ticker, 'yahoo', start=secondtime, end=thirdtime)
+#ADX calculation
 s['UpMove'] = s['High'] - s['High'].shift(1)
 s['DownMove'] = s['Low'] - s['Low'].shift(1)
 s['LogRet'] = np.log(s['Adj Close']/s['Adj Close'].shift(1)) 
@@ -81,17 +98,26 @@ s['PDM'] = s['PDM'][s['PDM'] > 0]
 s['MDM'] = s['MDM'][s['MDM'] > 0]
 s['PDM'] = s['PDM'].fillna(0)
 s['MDM'] = s['MDM'].fillna(0)
+#For number of iterations
 for r in ranger1:
+    #Iteration tracking
     print(counter)
     counter = counter + 1
+    #Get random params and generated metrics
     holder = DefModADXStratOpt(ranger2, s)
+    #Add to dataframe
     DS2W = pd.concat([DS2W, holder], axis = 1)
+#End timer
 end2 = t.time()
+#Timer stats
 print('Dataset 2 is optimized, it took',end2-start2,'seconds') #run time in seconds
-#Next
+#DS3W
 counter = 0
+#Start timer
 start3 = t.time()
+#Request data
 s = data.DataReader(ticker, 'yahoo', start=thirdtime, end=lasttime)
+#ADX calculation
 s['UpMove'] = s['High'] - s['High'].shift(1)
 s['DownMove'] = s['Low'] - s['Low'].shift(1)
 s['LogRet'] = np.log(s['Adj Close']/s['Adj Close'].shift(1)) 
@@ -109,17 +135,26 @@ s['PDM'] = s['PDM'][s['PDM'] > 0]
 s['MDM'] = s['MDM'][s['MDM'] > 0]
 s['PDM'] = s['PDM'].fillna(0)
 s['MDM'] = s['MDM'].fillna(0)
+#For number of iterations
 for r in ranger1:
+    #Iteration tracking
     print(counter)
     counter = counter + 1
+    #Get random params and generated metrics
     holder = DefModADXStratOpt(ranger2, s)
+    #Add to dataframe
     DS3W = pd.concat([DS3W, holder], axis = 1)
+#End timer
 end3 = t.time()
+#Timer stats
 print('Dataset 3 is optimized, it took',end3-start3,'seconds')
-#this will correspond to DS4W
+#DS4W
 counter = 0
+#Start timer
 start4 = t.time()
+#Request data
 s = data.DataReader(ticker, 'yahoo', start=fourthtime, end=lasttime)
+#ADX calculation
 s['UpMove'] = s['High'] - s['High'].shift(1)
 s['DownMove'] = s['Low'] - s['Low'].shift(1)
 s['LogRet'] = np.log(s['Adj Close']/s['Adj Close'].shift(1)) 
@@ -137,39 +172,47 @@ s['PDM'] = s['PDM'][s['PDM'] > 0]
 s['MDM'] = s['MDM'][s['MDM'] > 0]
 s['PDM'] = s['PDM'].fillna(0)
 s['MDM'] = s['MDM'].fillna(0)
+#For number of iterations
 for r in ranger1:
+    #Iteration tracking
     print(counter)
     counter = counter + 1
+    #Get random params and generated metrics
     holder = DefModADXStratOpt(ranger2, s)
+    #Add to dataframe
     DS4W = pd.concat([DS4W, holder], axis = 1)
+#End timer
 end4 = t.time()
 print('Dataset 4 is optimized, it took',end4-start4,'seconds') #run time in seconds
-#define off period test sets
+
+#Define out of sample period test sets
 S1TS = pd.DataFrame()
 S2TS = pd.DataFrame()
 S3TS = pd.DataFrame()
 S4TS = pd.DataFrame()
-#remove duplicate columns
+#Remove duplicate columns
 DS1W = DS1W.loc[:,~DS1W.columns.duplicated()]
 DS2W = DS2W.loc[:,~DS2W.columns.duplicated()]
 DS3W = DS3W.loc[:,~DS3W.columns.duplicated()]
 DS4W = DS4W.loc[:,~DS4W.columns.duplicated()]
-#merge winners to create test sets
+#Merge winners to create test sets
 S1TS = pd.concat([S1TS, DS2W, DS3W, DS4W], axis = 1)
 S2TS = pd.concat([S2TS, DS1W, DS3W, DS4W], axis = 1)
 S3TS = pd.concat([S3TS, DS1W, DS2W, DS4W], axis = 1)
 S4TS = pd.concat([S4TS, DS1W, DS2W, DS3W], axis = 1)
-#dulpicates??
+#Remove duplicate columns
 S1TS = S1TS.loc[:,~S1TS.columns.duplicated()]
 S2TS = S2TS.loc[:,~S2TS.columns.duplicated()]
 S3TS = S3TS.loc[:,~S3TS.columns.duplicated()]
 S4TS = S4TS.loc[:,~S4TS.columns.duplicated()]
-#test the test sets
+#Test the test sets
 testset1winners = ModADXAggMaker(ticker, S1TS, firsttime, secondtime)
 testset2winners = ModADXAggMaker(ticker, S2TS, secondtime, thirdtime)
 testset3winners = ModADXAggMaker(ticker, S3TS, thirdtime, lasttime)
 testset4winners = ModADXAggMaker(ticker, S4TS, fourthtime, lasttime)
+#Dataframe for params that pass all test sets
 Aggregate = pd.DataFrame()
 Aggregate = pd.concat([Aggregate, testset1winners, testset2winners,
                     testset3winners, testset4winners],axis = 1)
+#Total optimal param sets
 Aggregate = Aggregate.loc[:,~Aggregate.columns.duplicated()]
