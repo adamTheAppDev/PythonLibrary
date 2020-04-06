b71c768487a7a3a16735285c3791920e16b0cf3c
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-04-05 22:44:04 -0700

    Update KthFoldModADXAdviceGiver.py

--- KthFoldModADXAdviceGiver.py
+++ KthFoldModADXAdviceGiver.py
@@ -1,17 +1,21 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Apr 11 23:20:32 2017
 
-@author: AmatVictoriaCuramIII
+@author: Adam Reinhold Von Fisher - https://www.linkedin.com/in/adamrvfisher/
+
 """
 
 #This is part of a kth fold optimization tool
 
+#Define function
 def DefModADXAdviceGiver(Aggregate, q):
+    #Import modules
     import numpy as np
     import pandas as pd
+    #Assign variables
     base = 0
     size = len(Aggregate.iloc[0])    
+    #Calculate ADX
     q['UpMove'] = q['High'] - q['High'].shift(1)
     q['DownMove'] = q['Low'] - q['Low'].shift(1)
     q['LogRet'] = np.log(q['Adj Close']/q['Adj Close'].shift(1)) 
@@ -29,14 +33,18 @@ def DefModADXAdviceGiver(Aggregate, q):
     q['MDM'] = q['MDM'][q['MDM'] > 0]
     q['PDM'] = q['PDM'].fillna(0)
     q['MDM'] = q['MDM'].fillna(0)
+    #Variable assignment
     advice = float()
-    for i in Aggregate:             
+    #For every param set
+    for i in Aggregate: 
+        #Assign params
         aa = Aggregate.loc[0,i] #numer of days for moving average window
         a = aa.astype(int)
         b = Aggregate[i].iloc[1]
         c = Aggregate[i].iloc[2] 
         d = Aggregate[i].iloc[3]
         window = a
+        #Calculate ADX
         q['AverageTrueRange'] = q['TrueRange'].rolling(window = window,
                                         center=False).sum()
         q['AverageTrueRange'] = ((q['AverageTrueRange'].shift(1)*(window-1
@@ -58,9 +66,12 @@ def DefModADXAdviceGiver(Aggregate, q):
         q['DX'] = q['DX'].fillna(0)
         q['ADX'] = q['DX'].rolling(window = window, center = False).mean()
         q['ADXmean'] = q['ADX'].mean() * b
+        
+        #Trim time series
 #        trim = (window * 2 - 1)
 #        q = q[trim:]
 #        replace = q[:trim]
+        #Directional methodology
         q['Touch'] = np.where(q['DIdivergence'] < c, 1,0) #long signal
         q['Touch'] = np.where(q['DIdivergence'] > d, -1, q['Touch']) #short signal
         q['Sustain'] = 0
@@ -68,14 +79,20 @@ def DefModADXAdviceGiver(Aggregate, q):
         q['Sustain'] = np.where(q['ADX'] < q['ADXmean'], (q['Touch']*-1
                               ), q['Sustain']) #never actually true when optimized
         q['Regime'] = q['Touch'] + q['Sustain']
+        #Apply position to returns
         q['Strategy'] = (q['Regime']).shift(1)*q['LogRet']
         q['Strategy'] = q['Strategy'].fillna(0)
+        #Constraints
         if len(q) <= 1:
 #            q = replace.append(q)
             continue
+        #Directional assumption
         toadd = q['Regime'].iloc[-1]
+        #Iteratively add to aggregate directional assumption stat
         base = base + toadd 
+        #Directional assumption statistic 
         advice = base/size
 #        q = replace.append(q)
 #        print(base)
+    #Output
     return advice
