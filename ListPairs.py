76796c8e07ceb0db176d5c31539a36949fc2f48f
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-04-09 18:24:37 -0700

    Update ListPairs.py

--- ListPairs.py
+++ ListPairs.py
@@ -7,8 +7,13 @@
 #This is an organizational tool that pairs items in list into a comprehensive list of all possible pairs.
 #For use in SpeedDater_ apps
 
+#Define function
 def ListPairs(tickers):
+    #Import modules
     import itertools as it
+    #Combination object
     subList = it.combinations(tickers, 2)
+    #Object to list
     fullList = list(subList)
+    #Output list
     return fullList
