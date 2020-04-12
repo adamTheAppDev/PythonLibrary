12cd6ba4f4127508a60149ed060a44b81ff6a988
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-04-12 11:56:52 -0700

    Update ModDatabaseGrabber.py

--- ModDatabaseGrabber.py
+++ ModDatabaseGrabber.py
@@ -1,22 +1,28 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jun  8 12:37:08 2017
 
-@author: Adam Reinhold Von Fisher - adamrvfisher@gmail.com 
-linkedin.com/in/adamrvfisher
+@author: Adam Reinhold Von Fisher - https://www.linkedin.com/in/adamrvfisher/
+
 """
 
 #This is a modified version of DatabaseGrabber - a query tool
 
+#Define fucntion
 def ModDatabaseGrabber(ticker):    
+    #Import modules
     import pandas as pd 
     #It will be neccessary for you to change the location of the working directory
-    #in order to load the data into the model. Please modify line 13 to match the destination 
+    #in order to load the data into the model. Please modify to match the destination 
     #of the working directory folder
-    dataframe = pd.read_pickle('F:\\Users\\Adam\\Desktop\\WorkingDirectory\\PriceHistory\\' + 
+    #Read in data
+    dataframe = pd.read_pickle('F:\\Users\\Username\\DirectoryLocation\\PriceHistory\\' + 
                   ticker)
+    #For all column's values
     for i in dataframe.columns:
+        #Change data type to numeric
         dataframe[i] =  pd.to_numeric(dataframe[i], errors='coerce')
+    #Delete duplicate columns + rows
     dataframe = dataframe.loc[:,~dataframe.columns.duplicated()]
     dataframe = dataframe[~dataframe.index.duplicated(keep='first')]
+    #Output table
     return dataframe
