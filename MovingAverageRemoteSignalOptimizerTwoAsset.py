2341bda63f3d571edf317863b42a071d8539cd29
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-04-13 19:25:16 -0700

    Update MovingAverageRemoteSignalOptimizerTwoAsset.py

--- MovingAverageRemoteSignalOptimizerTwoAsset.py
+++ MovingAverageRemoteSignalOptimizerTwoAsset.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Aug 30 19:07:37 2017
 
-@author: AmatVictoriaCuramIII
+@author: Adam Reinhold Von Fisher - https://www.linkedin.com/in/adamrvfisher/
+
 """
+
 
 #This is a brute force optimization, portfolio analysis, and trading strategy based on a 'remote' signal
 
+#Import modules
 import numpy as np
 import random as rand
 import pandas as pd
@@ -14,21 +16,21 @@ import time as t
 from DatabaseGrabber import DatabaseGrabber
 from YahooGrabber import YahooGrabber
 from pandas import read_csv
+
+#Empty data structures
 Empty = []
 Dataset = pd.DataFrame()
 Portfolio = pd.DataFrame()
 Start = t.time()
 Counter = 0
 
-#Input
-
+#Assign tickers
 Ticker1 = 'UVXY'
 Ticker2 = '^VIX'
-
 #Remote Signal
 Ticker3 = '^VIX'
 
-#Here we go
+#Request data
 Asset1 = YahooGrabber(Ticker1)
 Asset2 = YahooGrabber(Ticker2)
 
@@ -37,15 +39,15 @@ Asset2 = YahooGrabber(Ticker2)
 #Asset2.Date = pd.to_datetime(Asset2.Date, format = "%m/%d/%Y") 
 #Asset2 = Asset2.set_index('Date')
 #Asset2 = Asset2.reindex(index=Asset2.index[::-1])
-
+#Time series trimmer
 #Asset1 = Asset1[:-8]
 
-#Remote Signal
+#Remote signal ticker
 #Asset3 = YahooGrabber(Ticker3)
 
 #Match lengths
 
-#Trimmer
+#Time series trimmer
 trim = abs(len(Asset1) - len(Asset2))
 if len(Asset1) == len(Asset2):
     pass
@@ -55,69 +57,79 @@ else:
     else:
         Asset2 = Asset2[trim:]
 
-
+#Copy
 Asset3 = Asset2
 
 #Asset3 = Asset3[-len(Asset2):]
 
 #Asset2 = Asset2[-600:]
-#
-##Log Returns
-#
+
+
+#Calculate log returns
 Asset1['LogRet'] = np.log(Asset1['Adj Close']/Asset1['Adj Close'].shift(1))
 Asset1['LogRet'] = Asset1['LogRet'].fillna(0)
 Asset2['LogRet'] = np.log(Asset2['Close']/Asset2['Close'].shift(1))
 Asset2['LogRet'] = Asset2['LogRet'].fillna(0)
-#
-##Prepare the remote controller
 Asset3['LogRet'] = np.log(Asset3['Adj Close']/Asset3['Adj Close'].shift(1))
 Asset3['LogRet'] = Asset3['LogRet'].fillna(0)
-#
-#Brute Force Optimization
+
+#Number of iterations for brute force optimization
 iterations = range(0, 1000)
+#For number of iterations
 for i in iterations:
+    #Iteration tracking
     Counter = Counter + 1
+    #Generate random parameters
     a = rand.random()
     b = 1 - a
     c = 0#rand.random()
     d = 0#rand.random()
+    #Constraint
     if c + d > 1:
         continue
+    #Generate random params
     e = rand.randint(3,100)
     window = int(e)
+    #Calculate SMA
     Asset3['MA'] = Asset3['Adj Close'].rolling(window=window, center=False).mean()
- 
+    #Position sizing
     Asset1['Position'] = a
     Asset1['Position'] = np.where(Asset3['Adj Close'].shift(1) > Asset3['MA'].shift(1),
-                                    c,a)                                    
+                                    c,a)         
+    #Apply position to returns
     Asset1['Pass'] = (Asset1['LogRet'] * Asset1['Position'])
+    #Position sizing
     Asset2['Position'] = b
     Asset2['Position'] = np.where(Asset3['Adj Close'].shift(1) > Asset3['MA'].shift(1),
                                     d,b)
+    #Apply position to returns
     Asset2['Pass'] = (Asset2['LogRet'] * Asset2['Position'])
     Portfolio['Asset1Pass'] = (Asset1['Pass']) * (-1) #Pass a short position
-    Portfolio['Asset2Pass'] = (Asset2['Pass']) #* (-1)
+    Portfolio['Asset2Pass'] = (Asset2['Pass']) #* (-1)#Pass a long position
+    #Cumulative returns
     Portfolio['LongShort'] = Portfolio['Asset1Pass'] + Portfolio['Asset2Pass']
+    #Constraint
     if Portfolio['LongShort'].std() == 0:    
         continue
-    
+    #Returns on $1
     Portfolio['Multiplier'] = Portfolio['LongShort'].cumsum().apply(np.exp)
+    
+    #Incorrectly calculated drawdown stat
     drawdown =  1 - Portfolio['Multiplier'].div(Portfolio['Multiplier'].cummax())
     MaxDD = max(drawdown)
 #    if MaxDD > float(.21): 
 #        continue
-    
+    #Performance metric
     dailyreturn = Portfolio['LongShort'].mean()
+    #Constraint
 #    if dailyreturn < .003:
 #        continue
-    
+    #Performance metric
     dailyvol = Portfolio['LongShort'].std()
     sharpe =(dailyreturn/dailyvol)
-    
-    Portfolio['Multiplier'] = Portfolio['LongShort'].cumsum().apply(np.exp)
-    drawdown =  1 - Portfolio['Multiplier'].div(Portfolio['Multiplier'].cummax())
-    MaxDD = max(drawdown)
+    #Iteration tracking
     print(Counter)
+    #Save params and metrics to list
     Empty.append(a)
     Empty.append(b)
     Empty.append(c)
@@ -127,53 +139,79 @@ for i in iterations:
     Empty.append(sharpe/MaxDD)
     Empty.append(dailyreturn/MaxDD)
     Empty.append(MaxDD)
+    #List to series
     Emptyseries = pd.Series(Empty)
-    Dataset[0] = Emptyseries.values
+    #Series to dataframe
     Dataset[i] = Emptyseries.values
+    #Clear list
     Empty[:] = [] 
     
+#Metric of choice    
 z1 = Dataset.iloc[6]
+#Threshold
 w1 = np.percentile(z1, 80)
-v1 = [] #this variable stores the Nth percentile of top performers
-DS1W = pd.DataFrame() #this variable stores your financial advisors for specific dataset
+v1 = [] #this variable stores the Nth percentile of top params
+DS1W = pd.DataFrame() #this variable stores your params for specific dataset
+#For all metrics
 for h in z1:
+    #If greater than threshold
     if h > w1:
+      #Add to list  
       v1.append(h)
+#For top metrics
 for j in v1:
+      #Find column ID of metric
       r = Dataset.columns[(Dataset == j).iloc[6]]    
+      #Add param set to dataframe
       DS1W = pd.concat([DS1W,Dataset[r]], axis = 1)
+#Top metric
 y = max(z1)
-k = Dataset.columns[(Dataset == y).iloc[6]] #this is the column number
+#Column ID of top metric
+k = Dataset.columns[(Dataset == y).iloc[6]]
+#Top param set
 kfloat = float(k[0])
+#End timer
 End = t.time()
+#Timer stats
 print(End-Start, 'seconds later')
+#Display top param set
 print(Dataset[k])
 
-
+#Read top param set to model
 window = int((Dataset[kfloat][4]))
+#Calculate SMA
 Asset3['MA'] = Asset3['Adj Close'].rolling(window=window, center=False).mean()   
-
+#Position sizing
 Asset1['Position'] = (Dataset[kfloat][0])
 Asset1['Position'] = np.where(Asset3['Adj Close'].shift(1) > Asset3['MA'].shift(1),
                                     Dataset[kfloat][2],Dataset[kfloat][0])
+#Apply position to returns
 Asset1['Pass'] = (Asset1['LogRet'] * Asset1['Position'])
+#Position sizing
 Asset2['Position'] = (Dataset[kfloat][1])
 Asset2['Position'] = np.where(Asset3['Adj Close'].shift(1) > Asset3['MA'].shift(1),
                                     Dataset[kfloat][3],Dataset[kfloat][1])
+#Apply position to returns
 Asset2['Pass'] = (Asset2['LogRet'] * Asset2['Position'])
-
+#Pass returns to portfolio
 Portfolio['Asset1Pass'] = Asset1['Pass'] * (-1)
 Portfolio['Asset2Pass'] = Asset2['Pass'] #* (-1)
 #Portfolio['PriceRelative'] = Asset1['Adj Close'] / Asset2['Adj Close']
-#asone['PriceRelative'][-180:].plot(grid = True, figsize = (8,5))
+#Cumulative returns
 Portfolio['LongShort'] = Portfolio['Asset1Pass'] + Portfolio['Asset2Pass'] 
+#Graphical display 
 Portfolio['LongShort'][:].cumsum().apply(np.exp).plot(grid=True,
                                      figsize=(8,5))
+#Portfolio metrics
 dailyreturn = Portfolio['LongShort'].mean()
 dailyvol = Portfolio['LongShort'].std()
 sharpe =(dailyreturn/dailyvol)
+#Returns on $1
 Portfolio['Multiplier'] = Portfolio['LongShort'].cumsum().apply(np.exp)
+#Incorrectly calculated drawdown stat 
 drawdown2 =  1 - Portfolio['Multiplier'].div(Portfolio['Multiplier'].cummax())
 #conversionfactor = Portfolio['PriceRelative'][-1]
+#Incorrect stat
 print(max(drawdown2))
+#Optional save to pickle
 #pd.to_pickle(Portfolio, 'VXX:UVXY')
