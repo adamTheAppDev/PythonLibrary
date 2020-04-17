df36c4f6becf9f16804e3be2df1eed85d3b04dc6
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-04-16 18:55:59 -0700

    Update MultiThreadADXStratOptExecution.py

--- MultiThreadADXStratOptExecution.py
+++ MultiThreadADXStratOptExecution.py
@@ -1,17 +1,23 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Apr 18 14:52:56 2017
 
-@author: AmatVictoriaCuramIII
+@author: Adam Reinhold Von Fisher - https://www.linkedin.com/in/adamrvfisher/
+
 """
 
 #This is part of a multithreading tool to speed up brute force optimization - looks under construction
 
+#Import modules
 from multithreadADXStratOpt import multithreadADXStratOpt
 import numba as nb
 import time as t
+#Start timer
 start = t.time()
+#Assign function
 f_nb = (multithreadADXStratOpt)
+#End timer
 end = t.time()
+#Timer stats
 print(end-start,'seconds later..')
+#Print result
 print(f_nb)
