95545087e4a1e9669126495deb6994052bfe5b94
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-05-13 19:00:37 -0700

    Update RMultipleTrackerDonchianTrend+GraphII.py

--- RMultipleTrackerDonchianTrend+GraphII.py
+++ RMultipleTrackerDonchianTrend+GraphII.py
@@ -56,12 +56,12 @@ Slippage = .004
 Asset1 = Asset1[-1250:] #In
 
 #Variable windows
-#donchianwidow is used to find the min/max of the price range to make the long/short signal
+#Donchianwidow is used to find the min/max of the price range to make the long/short signal
 #Smaller donchain window = more likely double days
 donchianwindow = 16
 #ATRwindow is used for volatility position sizing
 ATRwindow = 32
-#stopwindow is used for trailing high/low used for long/short exits
+#Stopwindow is used for trailing high/low used for long/short exits
 stopwindow = 9
 #How many ATR distance between stop
 numATR = 1.46
@@ -71,7 +71,7 @@ Counter = 0
 #SubIndex column is a secondary index, it only exists to help identify exits
 Asset1['SubIndex'] = range(0,len(Asset1))
 
-#Log Returns
+#Calculate log Returns
 Asset1['LogRet'] = np.log(Asset1['Adj Close']/Asset1['Adj Close'].shift(1))
 Asset1['LogRet'] = Asset1['LogRet'].fillna(0)
 
@@ -95,9 +95,9 @@ Asset1['RollingShortStop'] = Asset1['High'].rolling(window = stopwindow, center 
 #Asset1[['RollingMax','RollingMin','Close']].plot()
 
 #Signal = Price </> min/max
-#if price is greater than the max go long
+#If price is greater than the max go long
 Asset1['LongSignal'] = np.where(Asset1['High'] > Asset1['RollingMax'].shift(1), 1, 0)
-#if price is less than the min go short
+#If price is less than the min go short
 Asset1['ShortSignal'] = np.where(Asset1['Low'] < Asset1['RollingMin'].shift(1), 1, 0)
 
 #If double signal days exist, then entry and P/L on those days will not be reflected correctly, spurious return stream
@@ -108,7 +108,7 @@ Asset1['DoubleDay'] = np.where(Asset1['LongSignal'] + Asset1['ShortSignal'] == 2
 Asset1['Signal'] = np.where(Asset1['LongSignal'] == 1, 1, 0)
 Asset1['Signal'] = np.where(Asset1['ShortSignal'] == 1, -1, Asset1['Signal'])
 
-#if Rolling Min/Max is still being computed, stay out of market
+#If Rolling Min/Max is still being computed, stay out of market
 Asset1['Signal'] = np.where(Asset1['RollingMax'] == np.nan, 0, Asset1['Signal'])
 
 #Index values for segmenting data for trade analysis
@@ -121,7 +121,7 @@ Asset1['TradeATR'] = np.where(Asset1['Signal'] != 0, Asset1['ATR'].shift(1), np.
 Asset1['EntryPriceUnitOne'] = np.nan
 Asset1['StopPriceUnitOne'] = np.nan
 
-#On the first signal we record entry, create stop regime, record  exit, record 
+#On the first signal record entry, create stop regime, record  exit, record 
 #trade details, and then trim the time series to the next signal after exit. This process repeats.    
 #TradeSubset is a copy of Asset1 from the date of the first signal to the end of the time series
 TradeSubset = Asset1.loc[(Asset1.index >= SignalDates[0])] 
@@ -129,7 +129,7 @@ TradeSubset = Asset1.loc[(Asset1.index >= SignalDates[0])]
 #Every trade is in the while loop. If a position exists that is still open
 #at the end of the testing period, it is taken care of outside the while loop
 #while Counter < 1: #Use this instead of the while loop to go a certain number of trades into the iteration
-#while there is still a signal in the time series
+#While there is still a signal in the time series
 while sum(abs(TradeSubset['Signal'])) != 0:
     #Reset gap indicators
     GapEntry = 0
@@ -168,7 +168,7 @@ while sum(abs(TradeSubset['Signal'])) != 0:
                                                         limit = (stopwindow - 1))
         #Add the trailing highest lows
         TradeSubset['StopPriceUnitOne'] = TradeSubset['StopPriceUnitOne'].fillna(TradeSubset['RollingLongStop'])
-        #We want the cumulative maximum; thus preventing the stop from moving down.
+        #Use the cumulative maximum; thus preventing the stop from moving down.
         TradeSubset['StopPriceUnitOne'] = TradeSubset['StopPriceUnitOne'].cummax()
         
         #For every day the trade is open
@@ -191,7 +191,7 @@ while sum(abs(TradeSubset['Signal'])) != 0:
             ExitPrice = TradeSubset['Open'].loc[IndexOfExit]
             #Record exit gap
             GapExit = 1
-            #Slippage and commission go here.
+        #Calculate returns - slippage and commission go here.
         TradePercentReturn = (ExitPrice - EntryPrice) / ((RiskPerTrade ** -1) * DollarRisk)
         TradeDollarReturn = (ExitPrice - EntryPrice) * numshares
     
@@ -215,9 +215,9 @@ while sum(abs(TradeSubset['Signal'])) != 0:
         #Forward fill stop
         TradeSubset['StopPriceUnitOne'] = TradeSubset['StopPriceUnitOne'].ffill(
                                                         limit = (stopwindow - 1))
-        #Add the trailing lowest highs
+        #Add the trailing lost highs
         TradeSubset['StopPriceUnitOne'] = TradeSubset['StopPriceUnitOne'].fillna(TradeSubset['RollingShortStop'])
-        #We want the cumulative maximum; thus preventing the stop from moving down.
+        #Use the cumulative maximum; thus preventing the stop from moving down.
         TradeSubset['StopPriceUnitOne'] = TradeSubset['StopPriceUnitOne'].cummin()
         
         #For every day the trade is open
@@ -239,12 +239,13 @@ while sum(abs(TradeSubset['Signal'])) != 0:
             ExitPrice = TradeSubset['Open'].loc[IndexOfExit]
             #Record gap exit            
             GapExit = 1
+        #Calculate returns - slippage and commission go here.    
         TradePercentReturn = (EntryPrice - ExitPrice) / ((RiskPerTrade ** -1) * DollarRisk)
         TradeDollarReturn = (EntryPrice - ExitPrice) * numshares
     #If there is no exit signal, the trade is still open..    
     if math.isnan(ExitPrice) == True:
             break
-        
+    #Number of periods in trade    
     LengthOfTrade = TradeSubset['SubIndex'].loc[IndexOfExit] - TradeSubset['SubIndex'].loc[IndexOfEntry]
     #The SubIndex of the exit date is for continuing looking for rentry in new trade subset 
     SubIndexOfExit = TradeSubset['SubIndex'].loc[IndexOfExit]
@@ -328,7 +329,7 @@ if sum(abs(TradeSubset['Signal'])) != 0:
                                                         limit = (stopwindow - 1))
         #Add the trailing highest lows
         TradeSubset['StopPriceUnitOne'] = TradeSubset['StopPriceUnitOne'].fillna(TradeSubset['RollingLongStop'])
-        #We want the cumulative maximum; thus preventing the stop from moving down.
+        #Use the cumulative maximum; thus preventing the stop from moving down.
         TradeSubset['StopPriceUnitOne'] = TradeSubset['StopPriceUnitOne'].cummax()
         
         #For every day the trade is open
@@ -340,7 +341,7 @@ if sum(abs(TradeSubset['Signal'])) != 0:
                 break          
             else:
                 IndexOfExit = TradeSubset.index[-1]
-            
+        #Calculate returns - slippage and commission go here    
         TradePercentReturn = (ExitPrice - EntryPrice) / ((RiskPerTrade ** -1) * DollarRisk)
         TradeDollarReturn = (ExitPrice - EntryPrice) * numshares
     
@@ -366,7 +367,7 @@ if sum(abs(TradeSubset['Signal'])) != 0:
                                                         limit = (stopwindow - 1))
         #Add the trailing lowest highs
         TradeSubset['StopPriceUnitOne'] = TradeSubset['StopPriceUnitOne'].fillna(TradeSubset['RollingShortStop'])
-        #We want the cumulative maximum; thus preventing the stop from moving down.
+        #Use the cumulative minimum; thus preventing the stop from moving down.
         TradeSubset['StopPriceUnitOne'] = TradeSubset['StopPriceUnitOne'].cummin()
         
         #For every day the trade is open
@@ -380,13 +381,13 @@ if sum(abs(TradeSubset['Signal'])) != 0:
                 IndexOfExit = TradeSubset.index[-1]
 
         ExitPrice = TradeSubset['Adj Close'][-1]
-        
+        #Calculate returns - slippage and commission go here
         TradePercentReturn = (EntryPrice - ExitPrice) / ((RiskPerTrade ** -1) * DollarRisk)
         TradeDollarReturn = (EntryPrice - ExitPrice) * numshares
     #If there is no exit signal, the trade is still open..    
     if math.isnan(ExitPrice) == True:
             pass
-        
+    #Number of periods in trade    
     LengthOfTrade = TradeSubset['SubIndex'].loc[IndexOfExit] - TradeSubset['SubIndex'].loc[IndexOfEntry]
     #The SubIndex of the exit date is for continuing looking for rentry in new trade subset 
     SubIndexOfExit = TradeSubset['SubIndex'].loc[IndexOfExit]
@@ -430,14 +431,12 @@ Trades = Trades.rename(index={0: "TradeDirection", 1: "IndexOfEntry", 2: "IndexO
     8: "ExitPrice", 9: "numshares", 10: "TradeATR",
     11: "TradePercentReturn", 12: "TradeDollarReturn", 13:"DollarRisk", 
     14: "RMultiple"})
-    
+#Zeros and ones
 Asset1['StrategyPercentReturns'] = 1   
 Asset1['StrategyDollarReturns'] = 0   
 
-#The next two lines are the only reason keep track of 'SubIndexOfExit'
-#This is where I apply the returns on the trade close date to get the trade by trade return stream
+#Apply the returns on the trade close date to get the trade by trade return stream
 for d in Trades:
-#    Asset1['StrategyPercentReturns'].loc[(Asset1['SubIndex'] == Trades[d]['SubIndexOfExit'])] = 1 + Trades[d]['TradePercentReturn']
     Asset1['StrategyDollarReturns'].loc[(Asset1['SubIndex'] == Trades[d]['SubIndexOfExit'])] = 1 + Trades[d]['TradeDollarReturn']
 #System statistics    
 #Number Win/Loss
@@ -464,20 +463,7 @@ Asset1['DollarPL'].plot()
 print(sum(Asset1['DoubleDay']), ' Double signal days exist')
 print('The expectancy of the system is ', Expectancy)
 print(RMultiples)
-#Need to make histogram
-#plt.hist(RMultiples['RMultiple'], normed=True, bins=10
-#plt.ylabel('RMultiple')
-#plt.show()
 
-#Recreating stoploss and adding scatter for stop outs
-#Asset1['Stoploss'] = 0
-#For every trade
-#for tr in Trades:
-#Based on direction place gap adjusted stop loss into Stoploss column
-#    pass
-#Based on direction forward fill stoploss until exit
-#Make directionally colored scatter for trade exit
-#
 #All graphing occuring through separately populated dataframe copy
 #Make column that represents X axis 
 Asset1['Index'] = Asset1.index
@@ -493,9 +479,6 @@ plt.xlabel('Date')
 #Overlay for Donchian Channels
 axe.plot(AssetCopy['IndexToNumber'], Asset1['RollingMax'], color = 'green', label = 'RollingMax')
 axe.plot(AssetCopy['IndexToNumber'], Asset1['RollingMin'], color = 'red', label = 'RollingMin')
-#axe.plot(AssetCopy['IndexToNumber'], Asset1['ShortExitPrice'], color = 'magenta', label = 'ShortExitPrice')
-#axe.plot(AssetCopy['IndexToNumber'], Asset1['LongExitPrice'], color = 'blue', label = 'LongExitPrice')
-#axe.plot(AssetCopy['IndexToNumber'], Asset1['FilledStop'], color = 'black', label = 'FilledStop')
 #Overlay for dir dots
 #Signal triangles..
 EntryScatterData = pd.DataFrame()
@@ -514,7 +497,7 @@ ExitScatterData['ExitDates'] = Trades.iloc[2]
 ExitScatterData['IndexToNumber'] = ExitScatterData['ExitDates'].apply(mdates.date2num)
 ExitScatterData['TradeDirection'] = Trades.iloc[0]
 ExitScatterData['ExitPriceUnitOne'] = Trades.iloc[8]
-#Exit X
+#Exit X markers
 axe.scatter(ExitScatterData.loc[ExitScatterData['TradeDirection'] == -1, 'IndexToNumber'].values, 
         ExitScatterData.loc[ExitScatterData['TradeDirection'] == -1, 'ExitPriceUnitOne'].values, label='skitscat', color='green', s=75, marker="x")
 axe.scatter(ExitScatterData.loc[ExitScatterData['TradeDirection'] == 1, 'IndexToNumber'].values, 
