6c246d9312bd582b46b83d4a25bfb8ad2a204e0f
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-05-14 09:00:14 -0700

    Update SMAstrategy.py

--- SMAstrategy.py
+++ SMAstrategy.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Aug 30 19:07:37 2017
 
-@author: AmatVictoriaCuramIII
+@author: Adam Reinhold Von Fisher - https://www.linkedin.com/in/adamrvfisher/
+
 """
 
 #This is a strategy tester with a brute force optimizer
 
+#Import modules
 import numpy as np
 import random as rand
 import pandas as pd
@@ -14,25 +15,21 @@ import time as t
 from DatabaseGrabber import DatabaseGrabber
 from YahooGrabber import YahooGrabber
 from pandas import read_csv
+#Empty data structures
 Empty = []
 Dataset = pd.DataFrame()
 Portfolio = pd.DataFrame()
-Start = t.time()
 Counter = 0
 
-#Input
+#Start timer
+Start = t.time()
 
-Ticker1 = 'UVXY'
+#Assign tickers
+#Ticker1 = 'UVXY'
 #Ticker2 = '^VIX'
-
-#Remote Signal
 #Ticker3 = '^VIX'
 
-#Here we go
-#30MinUVXY
-#Asset1 = pd.read_csv('UVXYnew.csv')
-
-#Daily UVXY
+#Request data
 Asset1 = YahooGrabber(Ticker1)
 
 #For CC futures csv
@@ -41,43 +38,50 @@ Asset1 = YahooGrabber(Ticker1)
 #Asset2 = Asset2.set_index('Date')
 #Asset2 = Asset2.reindex(index=Asset2.index[::-1])
 
-
 #Out of Sample Selection
-Asset1 = Asset1[:-8]
+Asset1 = Asset1[:-252]
 
-##Log Returns
+#Calculate log Returns
 Asset1['LogRet'] = np.log(Asset1['Adj Close']/Asset1['Adj Close'].shift(1))
 Asset1['LogRet'] = Asset1['LogRet'].fillna(0)
 
-#Brute Force Optimization
+#Number of iterations for Brute Force Optimization
 iterations = range(0, 1000)
+#For number of iterations
 for i in iterations:
+    #Iteration tracking
     Counter = Counter + 1
+    #Generate random params
     a = 1
-    c = 0#rand.random()
-    e = 98#rand.randint(8,100)
+    c = rand.random()
+    e = rand.randint(8,100)
     window = int(e)
+    #Calculate simple moving average
     Asset1['MA'] = Asset1['Adj Close'].rolling(window=window, center=False).mean()
+    #Directional methodology
     Asset1['Regime'] = np.where(Asset1['MA'] < Asset1['Adj Close'], 1 , -1)                                 
+    #Apply position to returns
     Asset1['Strategy'] = (Asset1['LogRet'] * Asset1['Regime'].shift(1))
+    #Constraint
     #if Asset1['Strategy'].std() == 0:    
-    #    continue
-    
+        #continue
+    #Returns on $1
     Asset1['Multiplier'] = Asset1['Strategy'].cumsum().apply(np.exp)
+    #Incorrectly calculated max drawdown
     drawdown =  1 - Asset1['Multiplier'].div(Asset1['Multiplier'].cummax())
     MaxDD = max(drawdown)
+    #Constraint
     if MaxDD > float(.721): 
             continue
-    
+    #Performance metric
     dailyreturn = Asset1['Strategy'].mean()
-    #    if dailyreturn < .003:
-    #        continue
-    
+    #Constraint
+    #if dailyreturn < .003:
+            #continue
+    #Performance metrics
     dailyvol = Asset1['Strategy'].std()
     sharpe =(dailyreturn/dailyvol)
-        
-#Asset1['Strategy'][:].cumsum().apply(np.exp).plot(grid=True,
-#                                     figsize=(8,5))
+    #Save params and metric to list    
     print(Counter)
     Empty.append(a)
     Empty.append(c)
@@ -86,37 +90,61 @@ for i in iterations:
     Empty.append(sharpe/MaxDD)
     Empty.append(dailyreturn/MaxDD)
     Empty.append(MaxDD)
+    #List to series
     Emptyseries = pd.Series(Empty)
+    #Series to dataframe
     Dataset[i] = Emptyseries.values
+    #Clear list
     Empty[:] = [] 
-    
+#Metric of choice
 z1 = Dataset.iloc[3]
+#Threshold
 w1 = np.percentile(z1, 80)
-v1 = [] #this variable stores the Nth percentile of top performers
-DS1W = pd.DataFrame() #this variable stores your financial advisors for specific dataset
+v1 = [] #this variable stores the Nth percentile of top params
+DS1W = pd.DataFrame() #this variable stores your params for specific dataset
+#For all metrics
 for h in z1:
+    #If greater than threshold
     if h > w1:
+      #Add to list  
       v1.append(h)
+#For top metrics        
 for j in v1:
+      #Get column ID of metric
       r = Dataset.columns[(Dataset == j).iloc[3]]    
+      #Add to dataframe
       DS1W = pd.concat([DS1W,Dataset[r]], axis = 1)
+#Top metric    
 y = max(z1)
-k = Dataset.columns[(Dataset == y).iloc[3]] #this is the column number
+#Column ID of top metric
+k = Dataset.columns[(Dataset == y).iloc[3]]
+#Column ID of top metric - float
 kfloat = float(k[0])
+#End timer
 End = t.time()
+#Timer stats
 print(End-Start, 'seconds later')
+#Display top param set
 print(Dataset[k])
 
+#Read in params 
 window = int((Dataset[kfloat][2]))
+#Calculate simple moving average
 Asset1['MA'] = Asset1['Adj Close'].rolling(window=window, center=False).mean()   
+#Directional methodology
 Asset1['Regime'] = np.where(Asset1['MA'] < Asset1['Adj Close'], 1 , -1)
+#Apply position to returns
 Asset1['Strategy'] = (Asset1['LogRet'] * Asset1['Regime'].shift(1))
+#Graphical display
 Asset1['Strategy'][:].cumsum().apply(np.exp).plot(grid=True,
                                      figsize=(8,5))
+#Performance metrics
 dailyreturn = Asset1['Strategy'].mean()
 dailyvol = Asset1['Strategy'].std()
-sharpe =(dailyreturn/dailyvol)
+sharpe = (dailyreturn / dailyvol)
+#Returns on $1
 Asset1['Multiplier'] = Asset1['Strategy'].cumsum().apply(np.exp)
+#Incorrectly calculated max drawdown
 drawdown =  1 - Asset1['Multiplier'].div(Asset1['Multiplier'].cummax())
-
+#Display results
 print(max(drawdown))
