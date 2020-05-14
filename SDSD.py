ca20740b3908480f24dcdb50721a0a238d3db5d4
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-05-14 07:41:28 -0700

    Update SDSD.py

--- SDSD.py
+++ SDSD.py
@@ -1,19 +1,27 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Feb 28 13:24:51 2017
 
-@author: AmatVictoriaCuramIII
+@author: Adam Reinhold Von Fisher - https://www.linkedin.com/in/adamrvfisher/
+
 """
 
 #This is a summary statistic + database query tool
 #pandas_datareader is deprecated, use YahooGrabber
 
+#Import modules
 import numpy as np
 import pandas.io.data as web
 import pandas as pd
+
+#Define function
 def SDSD(s):
+    #Request data
     s = web.get_data_yahoo(s, start='1/1/1900', end='01/01/2018')
+    #Calculate log returns
     s['LogRet'] = np.log(s['Adj Close']/s['Adj Close'].shift(1))
+    #Calculate standard deviation
     s['SD'] = np.sqrt(pd.rolling_var(s['LogRet']*np.sqrt(252), window=252))
+    #Calculate standard deviation of standard deviation
     s['SDSD'] = np.std(s['SD']*np.sqrt(252))
+    #Output
     return s['SDSD'].tail(1)
