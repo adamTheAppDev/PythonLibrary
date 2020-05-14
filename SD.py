145a44eb68238aafd00dce886997a8e261eca56f
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-05-14 07:40:16 -0700

    Update SD.py

--- SD.py
+++ SD.py
@@ -1,16 +1,23 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Feb 28 13:05:58 2017
 
-@author: AmatVictoriaCuramIII
+@author: Adam Reinhold Von Fisher - https://www.linkedin.com/in/adamrvfisher/
+
 """
 
 #This is a summary statistic + database query tool
 
+#Import modules
 import numpy as np
 from pandas_datareader import data
+
+#Define function
 def SD(s):
+    #Request data
     s = data.DataReader(s, 'yahoo', start='1/1/1900', end='01/01/2050')
+    #Calculate log returns
     s['LogRet'] = np.log(s['Adj Close']/s['Adj Close'].shift(1))
+    #Calculate standard deviation
     s['SD'] = np.std(s['LogRet'])*np.sqrt(252)
+    #Output
     return s['SD'].tail(1)
