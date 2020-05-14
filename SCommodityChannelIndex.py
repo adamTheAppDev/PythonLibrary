4a01eb2c33e329c7a01073f57db8216eb265e297
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-05-14 07:39:17 -0700

    Update SCommodityChannelIndex.py

--- SCommodityChannelIndex.py
+++ SCommodityChannelIndex.py
@@ -1,18 +1,24 @@
+# -*- coding: utf-8 -*-
 """
-Spyder Editor
+
+@author: Adam Reinhold Von Fisher - https://www.linkedin.com/in/adamrvfisher/
+
 """
 
 #This is a technical analysis + database query tool
 
+#Define function
 def SCommodityChannelIndex(s):
+    #Assign params
     constant = .02
     SMAwindow = 20
+    #Calculate typical price
     s['TP'] = (s['High'] + s['Low'] + s['Adj Close']) / 3
+    #Calculate SMA of typical price
     s['TPSMA'] = s['TP'].rolling(center=False, window = SMAwindow).mean()
+    #Calculate standard deviation of TP
     s['MeanDeviation'] = s['TP'].rolling(center=False, window = SMAwindow).std()
+    #Calculate commodity channel index
     s['CCI'] = ((s['TP'] - s['TPSMA'])/(constant*s['MeanDeviation']))
+    #Output
     return s['CCI'][-1]
-#    s['Top'] = 100
-#    s['Bottom'] = -100
-#    s = s[SMAwindow:]
-#    s[['CCI','Top','Bottom']].plot(grid = True, figsize = (8,3))
