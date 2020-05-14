0df9c3223cfb471eae4091e5eb278071d6a0d9ba
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-05-13 21:22:09 -0700

    Update ReqContractDetailsIBAPI.py

--- ReqContractDetailsIBAPI.py
+++ ReqContractDetailsIBAPI.py
@@ -1,8 +1,9 @@
+  
 # -*- coding: utf-8 -*-
 """
-Created on Mon Dec 10 18:35:21 2018
 
-@author: AmatVictoriaCuramIII
+@author: Adam Reinhold Von Fisher - https://www.linkedin.com/in/adamrvfisher/
+
 """
 
 #This is an API app for Interactive Brokers API, it requests and prints line by line data to screen.
@@ -12,10 +13,10 @@ Created on Mon Dec 10 18:35:21 2018
 #API Access
 #Double check your CWD
 import os
-os.chdir('C:/source/pythonclient/')
+os.chdir('Z:/DirectoryLocation/pythonclient/')
 print(os.getcwd())
 
-#Add dependencies
+#Import modules
 from ibapi.wrapper import EWrapper
 from ibapi.client import EClient
 from ibapi.contract import Contract
@@ -24,31 +25,36 @@ import queue
 import datetime
 import time
 
+#Create app class w/ inheritance
 class TestApp(EWrapper, EClient):
+    #Overwriting
     def __init__(self):
         EClient.__init__(self, self)
-    
+    #Overwriting
     def error(self, reqId, errorCode, errorString):
         print("Error: ", reqId, " ", errorCode, " ", errorString)
-        
+    #Overwriting    
     def contractDetails(self, reqId, contractDetails):
         print("contractDetails: ", reqId, " ", contractDetails)
-
+#Create main function
 def main():
+    #Call test app
     app = TestApp()
-    
+    #Call to connect
     app.connect("127.0.0.1", 7497, 0)
-        
+    #Create contract object    
     contract = Contract()
+    #Define contract object
     contract.symbol = "AAPL"
     contract.secType = "STK"
     contract.exchange = "SMART"
     contract.currency = "USD"
     contract.primaryExchange = "NASDAQ"
-
+    #Request contract details
     app.reqContractDetails(1, contract)   
-
+    #Initialize
     app.run()
     
+#Run    
 if __name__ == "__main__":
     main()
