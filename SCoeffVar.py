f16d73f1f9aeba2682c09a62beac465089857533
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-05-14 07:37:27 -0700

    Update SCoeffVar.py

--- SCoeffVar.py
+++ SCoeffVar.py
@@ -1,16 +1,23 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Feb 28 13:28:23 2017
 
-@author: AmatVictoriaCuramIII
+@author: Adam Reinhold Von Fisher - https://www.linkedin.com/in/adamrvfisher/
+
 """
 
 #This is a summary statistic + database query tool
 
+#Import modules
 import numpy as np
+#Define function
 def SCoeffVar(s):
+    #Calculate log returns
     s['LogRet'] = np.log(s['Adj Close']/s['Adj Close'].shift(1))
+    #Calculate average returns
     s['Mean'] = np.mean(s['LogRet'])*252
+    #Calculate standard deviation
     s['SD'] = np.std(s['LogRet'])*np.sqrt(252)
+    #Calculate coefficient of variation
     s['CoeffVar'] = s['SD']/s['Mean']
+    #Output
     return s['CoeffVar'].tail(1)
