4fe587a352705c85e8a1a9087d8c628560143a9f
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-05-14 18:59:01 -0700

    Update ScanFilter.py

--- ScanFilter.py
+++ ScanFilter.py
@@ -1,33 +1,40 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Feb 28 23:29:44 2017
 
-@author: AmatVictoriaCuramIII
+@author: Adam Reinhold Von Fisher - https://www.linkedin.com/in/adamrvfisher/
+
 """
 
 #This is an older version of a database scanning/query tool
 
-#Get modules
+#Import modules
 from Age import Age
 from AverageReturn import AverageReturn
 from CoeffVar import CoeffVar
 from Trend import Trend
 from RelStrInd import RelStrInd
 from pandas import read_csv
-#Import and define info the list 
+
+#Read in CSV with symbols
 df = read_csv('bettersymbols.csv', sep = ',')
+#Set index
 df1 = df.set_index('Symbol')
+#Variable assignment
 symbols = df.Symbol.values
 port = (symbols)
 port1 = []
+#Display all tickers
 print(symbols)
+#For all symbols to scan
 for s in port:
     try:
+        #Search params
         if Age(s) > 1000 and sum(AverageReturn(s)) < -.1 and sum(CoeffVar(s)) < -.5 and sum(
                Trend(s)) < -0.04 and sum(RelStrInd(s)) > 60:
             print(s, AverageReturn(s), Trend(s), CoeffVar(s), Age(s), RelStrInd(s))
+            #Add to list
             port1.append(s)
     except OSError:
         pass
-#List refined portfolio
+#List symbols that pass scan
 print(port1)
