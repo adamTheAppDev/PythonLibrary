3bcd9920f7dbba358cc5f5053f52eb5fa16464dd
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-05-14 18:56:25 -0700

    Update SVR1Dimension.py

--- SVR1Dimension.py
+++ SVR1Dimension.py
@@ -1,30 +1,35 @@
-#This code was stolen from Arun Prakash on StackOverflow.com 
-#Arun wants to know how to predict additional rows, there is good material for him in the Muller and Guido python ML book
+# -*- coding: utf-8 -*-
+"""
+
+@author: Arun Prakash on StackOverflow.com
+
+"""
+
 #This is a one dimensional support vector regression
 
-#importing required libraries
+#import modules
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, LSTM
 
-#creating dataframe
+#Creating dataframe
 data = df.sort_index(ascending=True, axis=0)
 new_data = pd.DataFrame(index=range(0,len(df)),columns=['Date', 'Close'])
 for i in range(0,len(data)):
     new_data['Date'][i] = data['Date'][i]
     new_data['Close'][i] = data['Close'][i]
 
-#setting index
+#Setting index
 new_data.index = new_data.Date
 new_data.drop('Date', axis=1, inplace=True)
 
-#creating train and test sets
+#Creating train and test sets
 dataset = new_data.values
 
 train = dataset[0:987,:]
 valid = dataset[987:,:]
 
-#converting dataset into x_train and y_train
+#Converting dataset into x_train and y_train
 scaler = MinMaxScaler(feature_range=(0, 1))
 scaled_data = scaler.fit_transform(dataset)
 
@@ -36,7 +41,7 @@ x_train, y_train = np.array(x_train), np.array(y_train)
 
 x_train = np.reshape(x_train, (x_train.shape[0],x_train.shape[1],1))
 
-# create and fit the LSTM network
+#Create and fit the LSTM network
 model = Sequential()
 model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1],1)))
 model.add(LSTM(units=50))
@@ -45,7 +50,7 @@ model.add(Dense(1))
 model.compile(loss='mean_squared_error', optimizer='adam')
 model.fit(x_train, y_train, epochs=1, batch_size=1, verbose=2)
 
-#predicting 246 values, using past 60 from the train data
+#Predicting 246 values, using past 60 from the train data
 inputs = new_data[len(new_data) - len(valid) - 60:].values
 inputs = inputs.reshape(-1,1)
 inputs  = scaler.transform(inputs)
@@ -61,7 +66,7 @@ closing_price = scaler.inverse_transform(closing_price)
 rms=np.sqrt(np.mean(np.power((valid-closing_price),2)))
 print ("rms final 1:"+str(rms))
 
-#for plotting
+#For plotting
 train = new_data[:train_length]
 valid = new_data[train_length+10:]
 valid['Predictions'] = closing_price
