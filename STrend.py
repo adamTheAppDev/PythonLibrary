b759d0418ce8a1523cf6e331600165681c58f6ed
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-05-14 18:53:49 -0700

    Update STrend.py

--- STrend.py
+++ STrend.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Feb 28 13:26:20 2017
 
-@author: AmatVictoriaCuramIII
+@author: Adam Reinhold Von Fisher - https://www.linkedin.com/in/adamrvfisher/
+
 """
 
 #This is a technical analysis tool
 
-def STrend(s):# + denotes bullish, - denotes bearish
+#Define function; positive values = bullish, negative values = bearish
+def STrend(s):
+    #Calculate simple moving averages
     s['42d'] = s['Adj Close'].rolling(window=42, center=False).mean()
     s['252d'] = s['Adj Close'].rolling(window=252, center=False).mean()
+    #Difference / spread in points
     s['42-252'] = s['42d'] - s['252d']
+    #In percentage terms
     s['Trend'] = (s['42-252']/s['Adj Close']).dropna()
+    #Output
     return s['Trend'][-1]
