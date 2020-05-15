fa864f315ae095a1808d587a47e46a4ba7ab6d12
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-05-14 19:46:04 -0700

    Update SingleCSVFetchYahoo.py

--- SingleCSVFetchYahoo.py
+++ SingleCSVFetchYahoo.py
@@ -1,30 +1,44 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu May 18 15:55:54 2017
 
-@author: AmatVictoriaCuramIII
+@author: Adam Reinhold Von Fisher - https://www.linkedin.com/in/adamrvfisher/
+
 """
 
 #This is an HTML scraper + I/O + formatting tool
 
+#Import modules
 import requests
 from io import StringIO
 import pandas as pd
 import time as t
 from CrumbCatcher import CrumbCatcher
+
+#Start timer
 starttime = t.time()
+
+#Assign ticker
 ticker = "ACFS"
+
+#Get crumb
 artificialcrumb = str(CrumbCatcher(ticker))
+#Assign download URL
 downloadurl = ("https://query1.finance.yahoo.com/v7/finance/download/" + ticker 
 + "?period1=-631123200&period2=1598374000&interval=1d&events=history&crumb=" + artificialcrumb)
+#Post request
 response = requests.post(downloadurl)#, data=CookieDict)
+#Format respons
 datastr = response.text
 formatter = StringIO(datastr)
+#Assign to dataframe
 df = pd.read_csv(formatter, sep = ',')
+#Set index
 df = df.set_index('Date')
+#Format index
 df.index = pd.to_datetime(df.index, format = "%Y/%m/%d") 
+#End timer
 endtime = t.time()
+#Timer stats
 duration = endtime - starttime 
-
-#you may now write to file
-df.to_csv(("F:\\Users\\AmatVictoriaCuram\\TemporaryCSV\\"+ ticker + ".csv"))
+#Write to file
+df.to_csv(("F:\\Users\\Username\\DirectoryLocation\\"+ ticker + ".csv"))
