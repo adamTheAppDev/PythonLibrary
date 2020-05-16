32dffc8b66efafe5e4319765ca174e502c28da16
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-05-16 08:38:05 -0700

    Update TimeCheckIBAPI.py

--- TimeCheckIBAPI.py
+++ TimeCheckIBAPI.py
@@ -1,19 +1,21 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Dec 10 18:35:21 2018
 
-@author: AmatVictoriaCuramIII
+@author: Adam Reinhold Von Fisher - https://www.linkedin.com/in/adamrvfisher/
+
 """
 
-#This is a basic API app for Interactive Brokers API; can be found elsewhere online
+#This is a basic API app for Interactive Brokers API from IB API on youtube
 
 #API Access
-#Double check your CWD
+#Import module
 import os
-os.chdir('C:/source/pythonclient/')
+#Check your CWD
+os.chdir('Z:/sauce/pyclient/')
+#Display CWD
 print(os.getcwd())
 
-#Add dependencies
+#Import modules
 from ibapi.wrapper import EWrapper
 from ibapi.client import EClient
 from ibapi.contract import Contract
@@ -22,26 +24,30 @@ import queue
 import datetime
 import pandas as pd 
 
+#Define class
 class TestApp(EWrapper, EClient):
+    #Initialize
     def __init__(self):
         EClient.__init__(self, self)
-    
+    #Overwrite
     def error(self, reqId, errorCode, errorString):
         print("Error: ", reqId, " ", errorCode, " ", errorString)
-        
+    #Overwrite
     def contractDetails(self, reqId, contractDetails):
         print("contractDetails: ", reqId, " ", contractDetails)
 
-
+#Define function
 def main():
+    #Call test app
     app = TestApp()
-    
+    #Call to connect
     app.connect("127.0.0.1", 7497, 0) 
-
+    #Functionality
     print("serverVersion:%s connectionTime:%s" % (app.serverVersion(),
                                                       app.twsConnectionTime()))
-                                                      
+                                                  
     app.run()
     
+#Run    
 if __name__ == "__main__":
     main()
