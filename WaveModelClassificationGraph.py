d5b3211d5cc72be0789fe474e63fab3dcd9e0bdb
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-05-16 08:58:39 -0700

    Update WaveModelClassificationGraph.py

--- WaveModelClassificationGraph.py
+++ WaveModelClassificationGraph.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Oct  6 19:04:59 2019
 
-@author: AmatVictoriaCuramIII
+@author: Andreas Mueller and Guido
+
 """
 
-#This is a ML model
-#Supervised Learning Regression - from the Muller and Guido Python ML book
+#Supervised Learning Regression 
 
+#Import modules
 #import sys
 #import sklearn
 #import numpy as np
@@ -18,27 +18,28 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsRegressor
 import mglearn
-#generate data X and y, X is feature and y is target
+
+#Generate data X and y, X is feature and y is target
 X, y = mglearn.datasets.make_wave(n_samples=40)
 
-#split data - 'random' train/test selection
+#Split data - 'random' train/test selection
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 
-#instantiate the model and set the number of neighbors to consider to 3
+#Instantiate the model and set the number of neighbors to consider to 3
 reg = KNeighborsRegressor(n_neighbors=3)
-#fit the model using the training data and training targets
+#Fit the model using the training data and training targets
 reg.fit(X_train, y_train)
 
-#test set predictions
+#Test set predictions
 print("Test set predictions:\n{}".format(reg.predict(X_test)))
 
-#test set R^2
+#Test set R^2
 print("Test set R^2: {:.2f}".format(reg.score(X_test, y_test)))
 
-#graph features
+#Graph features
 plt.plot(X, y, 'o')
-#graph constraints
+#Graph constraints
 plt.ylim(-3, 3)
-#labels
+#Labels
 plt.xlabel("Feature")
 plt.ylabel("Target")
