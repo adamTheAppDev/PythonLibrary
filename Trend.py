47a29ac292cd23b9a51426145e7b0483693324ac
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-05-16 08:50:05 -0700

    Update Trend.py

--- Trend.py
+++ Trend.py
@@ -1,18 +1,27 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Feb 28 13:26:20 2017
 
-@author: AmatVictoriaCuramIII
+@author: Adam Reinhold Von Fisher - https://www.linkedin.com/in/adamrvfisher/
+
 """
 
 #This is a technical analysis tool
 
+#Import modules
 from pandas_datareader import data
-def Trend(s):# + denotes bullish, - denotes bearish
+
+#Define function
+def Trend(s):        #Positive values denotes bullish, negative values denotes bearish
+    #Request data
     s = data.DataReader(s, 'yahoo', start='1/1/1900', end='01/01/2050')
+    #Calculate simple moving averages
     s['42d'] = s['Adj Close'].rolling(window=42, center=False).mean()
     s['252d'] = s['Adj Close'].rolling(window=252, center=False).mean()
+    #Graphical display
     s[['Adj Close', '42d', '252d']].plot(grid=True, figsize=(8, 5))
+    #SMA spread
     s['42-252'] = s['42d'] - s['252d']
+    #Drop nans
     s['Trend'] = (s['42-252']/s['Adj Close']).dropna()
+    #Output
     return s['Trend'].tail(1)
