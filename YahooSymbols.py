cf5f43d5dfb4a903ac5f20818fddb05e3dd56f3f
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-05-16 10:47:59 -0700

    Update YahooSymbols.py

--- YahooSymbols.py
+++ YahooSymbols.py
@@ -1,18 +1,26 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat May 20 16:33:06 2017
 
-@author: AmatVictoriaCuramIII
+@author: Adam Reinhold Von Fisher - https://www.linkedin.com/in/adamrvfisher/
+
 """
 
 #This is an outdated database query tool
 
+#Import modules
 import os 
 import pandas as pd
-index = range(0,len(os.listdir('F:\\Users\\AmatVictoriaCuram\\Database')))
-goodsymbols = pd.DataFrame(os.listdir('F:\\Users\\AmatVictoriaCuram\\Database')
- ,columns = ['Symbol'])
+
+#Iterable
+index = range(0,len(os.listdir('Z:\\Users\\Username\\Database')))
+#Read in tickers
+goodsymbols = pd.DataFrame(os.listdir('Z:\\Users\\Username\\Database')
+                           , columns = ['Symbol'])
+#For all tickers
 for i in index:
+    #Add to dataframe
     goodsymbols['Symbol'][i] = goodsymbols['Symbol'][i]#[:-4]
+#Confirmation
 print(goodsymbols)
+#Save to CSV
 goodsymbols.to_csv('newgoodsymbols.csv',sep=',')
