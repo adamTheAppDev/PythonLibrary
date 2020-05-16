cff2089c07eb75020b92a4ab5a4792b80a5a0c0d
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-05-15 22:56:47 -0700

    Update TastyBracketLogic.py

--- TastyBracketLogic.py
+++ TastyBracketLogic.py
@@ -7,24 +7,6 @@ Created on Thu Dec 28 01:20:06 2017
 
 #This is an outdated trading model see DonchianTrendEfficiencyFilterSingleStockSingleFrequency.py
 
-#Main questions...
-#What is the best way to correctly simulate bracket orders?
-#How can I reflect changes in equity during the life of a trade?
-#What are the best practices for conducting model based strategy development?
-#What is a good way to document R multiples for the individual trades made?
-#What are, in real application, the most effective sampling methodologies? Kth fold, In/Out, none, etc? 
-#Appropriate slippage % for bad fills?
-#How can I reduce redundant assignments and other code inefficiencies?
-
-#Main constraints and assumptions
-#Assume that I have no idea whats going on here
-#Commissions and entry slippage
-#Adverse stop loss Gaps are closed at loss on open
-#Ending equity curve & statistics are spurious since equity changes during the life of a 
-#trade are not represented in the bracketed returns 
-
-#Bracket Logic w/ SMA Strategy
-
 #Import modules
 import numpy as np
 import random as rand
@@ -33,23 +15,24 @@ import time as t
 from DatabaseGrabber import DatabaseGrabber
 from YahooGrabber import YahooGrabber
 
-##Empty sets used for optimization
+#Empty sets used for optimization
 #Empty = []
 #Empty Dataframes
 #Dataset = pd.DataFrame()
 #Portfolio = pd.DataFrame()
-##Timing statistics and iteration counter for optimization
-#Start = t.time()
+
+#Iteration tracking
 #Counter = 0
+
+#Start tracking
 #start = t.time()
 
-#Inputs - OHLC data
+#Assign ticker
 Ticker1 = 'UVXY'
+#Request data
 Asset1 = YahooGrabber(Ticker1)
-#Tasty OHLC; ***ATTN*** insert path for OHLC data
-#Asset1 = pd.read_pickle('C:\\Users\\Tasty\\Desktop\\WorkingDirectory\\UVXY')
 
-#In percentages
+#Variable assignment
 LongStopLoss = .09
 LongProfitTake = .2
 ShortStopLoss = .09
@@ -57,17 +40,16 @@ ShortProfitTake = .2
 Commission = .01
 Slippage = .01
 
-#Time series trimmer for in/out sample data
+#Time series trimmer 
 Asset1a = Asset1[-1250:] #Out
 Asset1 = Asset1[:-1250] #In
-#
-#Log Returns
+
+#Calculate log Returns
 Asset1['LogRet'] = np.log(Asset1['Adj Close']/Asset1['Adj Close'].shift(1))
 Asset1['LogRet'] = Asset1['LogRet'].fillna(0)
 
 #SMA window
 window = 15
-
 #SMA calculation
 Asset1['MA'] = Asset1['Adj Close'].rolling(window=window, center=False).mean()
 
@@ -78,25 +60,24 @@ Asset1['Price/MA'] = Asset1['Adj Close']/Asset1['MA']
 Asset1['MA'] = Asset1['MA'].fillna(0)
 Asset1['Price/MA'] = Asset1['Price/MA'].fillna(0)
 
-#Signal = Price to Moving Average 
-#if price is greater than the MA go long w/ brackets
-#if price is less than the MA go short w/ brackets
+#Signal is Price to Moving Average 
 Asset1['Signal'] = np.where(Asset1['Price/MA'] >= 1, 1, -1)
 
-#if MA is still being computed, stay out of market
+#If MA is still being computed, stay out of market
 Asset1['Signal'] = np.where(Asset1['Price/MA'] == 0, 0, Asset1['Signal'])
 
-#Find the first trade of the signal period, so we can document entry price
+#Find the first trade of the signal period, document entry price
 Asset1['OriginalSignal'] = 0
 Asset1['OriginalSignal'].loc[Asset1['Signal'] != Asset1['Signal'].shift(1)] = Asset1['Signal']
+#Number of signals in time series
 numsignals = sum(abs(Asset1['OriginalSignal']))
 #Declare column to record entry price
 Asset1['EntryPrice'] = np.nan
 
-#If it's the original signal, record entry price
+#If original signal, record entry price
 Asset1['EntryPrice'].loc[(Asset1['OriginalSignal'] != 0)] = Asset1['Adj Close']
 
-#Assess spread/unfavorable fills here!
+#Assess spread/unfavorable fills here
 Asset1['EntryPriceSlippage'] = Asset1['EntryPrice']
 #Long slippage
 Asset1['EntryPriceSlippage'].loc[(Asset1['EntryPrice'] != 0) & (
@@ -105,7 +86,7 @@ Asset1['EntryPriceSlippage'].loc[(Asset1['EntryPrice'] != 0) & (
 Asset1['EntryPriceSlippage'].loc[(Asset1['EntryPrice'] != 0) & (
     Asset1['Signal'] == -1)] = Asset1['EntryPrice'] * (1 - Slippage)
 
-#Run the entry price DOWN the column until new position is taken
+#Forward fill down the column until new position is taken
 Asset1['EntryPriceSlippage'] = Asset1['EntryPriceSlippage'].ffill(inplace=False)
 #Fill nan with 0 for entry price
 Asset1['EntryPriceSlippage'] = Asset1['EntryPriceSlippage'].fillna(0)
@@ -134,28 +115,9 @@ Asset1['ProfitPrice'].loc[(Asset1['EntryPrice'] != 0) & (
 Asset1['ProfitPrice'] = Asset1['ProfitPrice'].ffill(inplace=False)
 Asset1['ProfitPrice'] = Asset1['ProfitPrice'].fillna(0)
 
-#MFE, MAE are used to calculate 'edge ratio'. May be useful for further strategy analysis
-#Declare - Max Favorable Excursion (favorable price movement from entry)
-#Asset1['MFE'] = 0
-
-#Declare - Max Adverse Excursion (adverse price movement from entry)
-#Asset1['MAE'] = 0
-#
-##MFE for long positions, MAE for short positions. Based on direction, entry price, daily high, and daily low
-#Asset1['LongGains'] = (Asset1['High'] - Asset1['EntryPrice'].shift(1))/Asset1['EntryPrice'].shift(1)
-#Asset1['LongGains'] = Asset1['LongGains'].replace([np.inf, -np.inf], np.nan)
-#Asset1['LongGains'] = Asset1['LongGains'].fillna(0)
-##MAE for long positions, MFE for short positions. Based on direction, entry price, daily high, and daily low
-#Asset1['ShortGains'] = (Asset1['EntryPrice'].shift(1) - Asset1['Low'])/Asset1['EntryPrice'].shift(1)
-#Asset1['ShortGains'] = Asset1['ShortGains'].replace([np.inf, -np.inf], np.nan)
-#Asset1['ShortGains'] = Asset1['ShortGains'].fillna(0)
-
-#We have to exit the trade... 
-#Redundant column assignment, need to consolidate
-
 #Is there an exit on this day? 1 = yes, 0 = no
 Asset1['Exit'] = 0
-#This will be the final return stream. Generally I use a regime of 
+#This will be the final return stream. Use a regime of 
 #(-1, or 0, or +1) multiplied by the next day's log return to get equity curve
 Asset1['BracketReturns'] = 1
 
@@ -180,10 +142,10 @@ Asset1['GapLTG'] = 0
 Asset1['GapLSL'] = 0
 
 #1 = STG being hit starting the day after the signal. After initial hit, 1s 
-#will run down the column even though the trade should be closed
+#will fill column even though the trade should be closed
 Asset1['STG'].loc[(Asset1['Signal'] == -1) & (
     Asset1['OriginalSignal'] == 0) & (Asset1['Low'] < Asset1['ProfitPrice'])] = 1    
-#find initial exit 
+#Find initial exit 
 Asset1['OriginalSTG'].loc[Asset1['STG'] != Asset1['STG'].shift(1)] = Asset1['STG']
 
 Asset1['LTG'].loc[(Asset1['Signal'] == 1) & (
@@ -222,7 +184,7 @@ Asset1['Exit'].loc[(Asset1['OriginalLSL'] == 1)] = 4 #exit as loss
 Asset1['Exit'].loc[(Asset1['OriginalSTG'] == 1)] = 5 #exit as gain
 Asset1['Exit'].loc[(Asset1['OriginalSSL'] == 1)] = 6 #exit as loss
 
-#Bring the brackets to life
+#Populate brackets
 #Assess Commissions on close of trade
 Asset1['BracketReturns'].loc[(Asset1['Exit'] == 1)] = (1 - LongStopLoss) - Commission
 Asset1['BracketReturns'].loc[(Asset1['Exit'] == 2)] = 1 - ShortStopLoss - Commission
@@ -243,25 +205,25 @@ Asset1['BracketReturns'].loc[(Asset1['GapLSL'] == 1)] = 1 + ((
 #no calculation for trading days where a position is held on open, but close signal is flat    
 #Asset1['Exit'].loc[(Asset1['Signal'] == 0)] = 0
 
+#Returns on $1
 Asset1['Multiplier'] = Asset1['BracketReturns'].cumprod()
 Asset1['Multiplier'].plot() 
+#Apply position to returns
 #Asset1['Strategy'] = (Asset1['LogRet'] * Asset1['NewSignal'].shift(1))
-#Asset1['Multiplier'] = Asset1['Strategy'].cumsum().apply(np.exp)
+#Incorrectly calculated max drawdown
 drawdown =  1 - Asset1['Multiplier'].div(Asset1['Multiplier'].cummax())
 MaxDD = max(drawdown)
-#
+#Performance metrics
 dailyreturn = (Asset1['BracketReturns'] - 1).mean()
-##
-#Dailyvol is going to be inaccurate because account equity is not calculated during holding period, only assessed on exit 
+#Dailyvol is inaccurate - account equity is not calculated during holding period, only assessed on exit 
 dailyvol = (Asset1['BracketReturns'] - 1).std()
 sharpe =(dailyreturn/dailyvol)
-##
-#Portfolio['Multiplier'] = Asset1['Strategy'].cumsum().apply(np.exp)
-#Portfolio['Multiplier'] = Portfolio['Multiplier'].fillna(1)
-#drawdown =  1 - Portfolio['Multiplier'].div(Portfolio['Multiplier'].cummax())
-#MaxDD = max(drawdown)
+#Display results
 #print(MaxDD)
-##end = t.time()
-##totaltime = end - start
-##print('Time taken = ', totaltime)
+#End timer
+#end = t.time()
+#Time stats
+#totaltime = end - start
+#print('Time taken = ', totaltime)
+#Graphical display
 #Portfolio['Multiplier'].plot()
