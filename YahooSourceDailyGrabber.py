4ad82898264abd82f8abac575624bc36e7e90f12
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-05-16 10:31:16 -0700

    Update YahooSourceDailyGrabber.py

--- YahooSourceDailyGrabber.py
+++ YahooSourceDailyGrabber.py
@@ -1,19 +1,23 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jan 22 22:41:00 2019
 
-@author: AmatVictoriaCuramIII
+@author: Adam Reinhold Von Fisher - https://www.linkedin.com/in/adamrvfisher/
+
 """
 
 #This is a local database query tool
 
-#Yahoo Daily Grabber
+#Define function
 def YahooSourceDailyGrabber(ticker):    
+    #Import modules
     import pandas as pd
-    dataframe = pd.read_pickle('F:\\Users\\AmatVictoriaCuram\\FDL\\DataSources\\' +
+    dataframe = pd.read_pickle('Z:\\Users\\Username\\DirectoryLocation\\DataSources\\' +
             'YahooSource\\ProcessedData\\' + 'DAY' + '\\' + 'DAY-' + ticker + '\\' + 'DAY-' + ticker)
+    #Make all columns numeric data type
     for i in dataframe.columns:
         dataframe[i] =  pd.to_numeric(dataframe[i], errors='coerce')
+    #Delete duplicate rows and columns    
     dataframe = dataframe.loc[:,~dataframe.columns.duplicated()]
     dataframe = dataframe[~dataframe.index.duplicated(keep='first')]
+    #Ouput
     return dataframe
