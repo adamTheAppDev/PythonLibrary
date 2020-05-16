cad86c16afbc74ea143883f56d24340f0a6f34a6
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-05-15 23:15:22 -0700

    Update TastyContangoSignalOpt.py

--- TastyContangoSignalOpt.py
+++ TastyContangoSignalOpt.py
@@ -1,48 +1,40 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Aug 30 19:07:37 2017
 
-@author: Adam Reinhold Von Fisher 
+@author: Adam Reinhold Von Fisher - https://www.linkedin.com/in/adamrvfisher/
+
 """
 
 #This is a two asset portfolio strategy tester + brute force optimizer for volatility
 
+#Import modules
 import numpy as np
 import random as rand
 import pandas as pd
 import time as t
 from DatabaseGrabber import DatabaseGrabber
 from YahooGrabber import YahooGrabber
+
+#Empty data strucutures
 Empty = []
 Dataset = pd.DataFrame()
 Portfolio = pd.DataFrame()
-Start = t.time()
 Counter = 0
 
-#Input
+#Start timer
+Start = t.time()
 
+#Assign tickers
 #Ticker1 = 'UVXY'
 #Ticker2 = '^VIX'
-#
-##Contango Signal will consist of VIX/VXV 
 #Ticker3 = '^VXV'
 
-#Grab data
-#Asset1 = YahooGrabber(Ticker1)
-#Asset2 = YahooGrabber(Ticker2)
-#
-##Remote Signal
-#Asset3 = YahooGrabber(Ticker3)
+#Request data
+Asset1 = YahooGrabber(Ticker1)
+Asset2 = YahooGrabber(Ticker2)
+Asset3 = YahooGrabber(Ticker3)
 
-#Grab data
-Asset1 = pd.read_pickle('TastyUVXY')
-
-Asset2 = pd.read_pickle('TastyVIX')
-
-Asset3 = pd.read_pickle('TastyVXV')
-
-#Match lengths
-#Trimmer - trim main dataframes to equal length
+#Time series trimmer
 trim = abs(len(Asset1) - len(Asset2))
 if len(Asset1) == len(Asset2):
     pass
@@ -52,12 +44,10 @@ else:
     else:
         Asset2 = Asset2[trim:]
 
-#Trim 
+#Match lengths        
 Asset3 = Asset3[-len(Asset2):]
 
-
-
-#Prepare log returns
+#Calculate log returns
 Asset1['LogRet'] = np.log(Asset1['Adj Close']/Asset1['Adj Close'].shift(1))
 Asset1['LogRet'] = Asset1['LogRet'].fillna(0)
 Asset2['LogRet'] = np.log(Asset2['Adj Close']/Asset2['Adj Close'].shift(1))
@@ -65,83 +55,70 @@ Asset2['LogRet'] = Asset2['LogRet'].fillna(0)
 Asset3['LogRet'] = np.log(Asset3['Adj Close']/Asset3['Adj Close'].shift(1))
 Asset3['LogRet'] = Asset3['LogRet'].fillna(0)
 
-#Prepare the remote signal, 1 month / 3 month  
-
+#Price relative
 Asset3['Contango'] = (Asset2['Close']/Asset3['Close'])
 
-##Retrim Assets
-#Asset1 = Asset1[window:]
-#Asset2 = Asset2[window:]                             
-#Asset3 = Asset3[window:]
-
-#Brute Force Optimization - 2000 iterations on Macbook takes about 14 seconds
+#Number of iterations for Brute Force Optimization
 iterations = range(0, 2000)
 
 for i in iterations:
-    #count the iterations....
+    #Iteration tracking 
     Counter = Counter + 1
-    #represents position in Asset1 = a, Asset2 = b
+    #Generate random params
     a = rand.random()
     b = 1 - a
-    #represents position in Asset1 = c, Asset2 = d, given signal e is 'true'
     c = rand.random()
     d = 1 - c
-    #e will represent a threshold for VIX/VXV where the positions a and b will
-    #be switched to c and d ----- 'opportunistic short for UVXY' 
-    #the strategy logic can be improved upon but it is not relevant for my questioning
     e = 1.5 - (rand.random()*.5)
     
-    #a is default position for UVXY
+    #Position sizing
     Asset1['Position'] = a
-    #when VIX/VXV rises above threshold e, use c instead of a for position in Asset1
+    #Alternative position sizing
     Asset1['Position'] = np.where(Asset3['Contango'].shift(1) > e,
                                     c,a)   
-    #we will 'pass' log returns multiplied by position size to the portfolio dataframe
+    #Apply position to returns
     Asset1['Pass'] = (Asset1['LogRet'] * Asset1['Position'])
-    #b is default position for VIX    
+    #Position sizing 
     Asset2['Position'] = b
-    #when VIX/VXV rises above threshold e, use d instead of b for position in Asset2
+    #Alternative position sizing
     Asset2['Position'] = np.where(Asset3['Contango'].shift(1) > e,
                                     d,b)
-    #we will 'pass' log returns multiplied by position size to the portfolio dataframe           
+    #Apply position to returns       
     Asset2['Pass'] = (Asset2['LogRet'] * Asset2['Position'])
-    
+    #Pass individual return streams to portfolio
     Portfolio['Asset1Pass'] = (Asset1['Pass']) * (-1) #Pass a short position on Asset1 to portfolio
     Portfolio['Asset2Pass'] = (Asset2['Pass']) #* (-1)
 
-    #Add respective 'pass' to get total return 
+    #Cumulative portfolio returns
     Portfolio['TotalRet'] = (Portfolio['Asset1Pass']) + (Portfolio['Asset2Pass'])
     
-    #avoid dividing by 0 for errors/shorten optimization time
+    #Constraint
     if Portfolio['TotalRet'].std() == 0:    
         continue
     
-    
+    #Returns on $1
     Portfolio['Multiplier'] = Portfolio['TotalRet'].cumsum().apply(np.exp)
     
-    #find drawdowns
+    #Incorrectly calculated drawdown
     drawdown =  1 - Portfolio['Multiplier'].div(Portfolio['Multiplier'].cummax())
     MaxDD = max(drawdown)
     
-    #eliminate unattractive drawrdown iterations/shorten optimization
+    #Constraint
     if MaxDD > float(.3): 
         continue
-    #eliminate unattractive return iterations/shorten optimization 
+    #Performance metric
     dailyreturn = Portfolio['TotalRet'].mean()
     if dailyreturn < .002:
         continue
 
-    #you get the idea
+    #Performance metrics
     dailyvol = Portfolio['TotalRet'].std()
-    
-    #well, no risk free rate of return..
     sharpe =(dailyreturn/dailyvol)
     
-    drawdown =  1 - Portfolio['Multiplier'].div(Portfolio['Multiplier'].cummax())
-    MaxDD = max(drawdown)
+    #Iteration tracking
     print(Counter)
     
-    #This part stores 'successful' iteration parameters in a list
+    #Add params and metrics to list
     Empty.append(a)
     Empty.append(b)
     Empty.append(c)
@@ -152,54 +129,77 @@ for i in iterations:
     Empty.append(dailyreturn/MaxDD)
     Empty.append(MaxDD)
     
-    #List to vertical series
+    #List series
     Emptyseries = pd.Series(Empty)
-    #Concatenate Vertical Series - Horizontally to Dataframe
+    #Series to dataframe
     Dataset[i] = Emptyseries.values
-    #Clear out the list for the next iteration! Rinse and repeat.
+    #Clear list 
     Empty[:] = [] 
     
-#Now we are outside the loop and want to grab an attractive iteration
-#z1 stores a horizonal series of sharpe/MaxDD statistics    
+#Metric of choice
 z1 = Dataset.iloc[6]
-#w1 is the threshold value for top 80 percentile of highest 'sharpe' to maxDD 
+#Threshold
 w1 = np.percentile(z1, 80)
-v1 = [] #this list stores the 80 to 100 percentile of highest 'sharpe' to maxDD from the first for loop
-DS1W = pd.DataFrame() #this variable stores your Vertical Series with parameters concatenated horizontally
-for h in z1: #for all 'sharpe' to maxDD parameters
-    if h > w1: #if you are above 80% threshold
-      v1.append(h) #you are part of v1
-for j in v1: #for all parameters in v1
-      r = Dataset.columns[(Dataset == j).iloc[6]] #grab the entire Vertical series of parameters associated with specific element of z1
-      DS1W = pd.concat([DS1W,Dataset[r]], axis = 1) # and concatenate that to an empty dataframe, just like the 'Dataset' variable
-y = max(z1) #find the winning-est parameter set based on max(sharpe/maxDD) from entire simulation
-k = Dataset.columns[(Dataset == y).iloc[6]] #this is the column number from Dataset
-kfloat = float(k[0]) #reference column number as float
+v1 = [] #this list stores Nth percentile of top metrics
+DS1W = pd.DataFrame() #this variable stores your parameters 
+#For all metrics
+for h in z1: 
+    #If greater than threshold
+    if h > w1: 
+      #Add to list  
+      v1.append(h) 
+#For top metrics        
+for j in v1: 
+    #Get column ID
+    r = Dataset.columns[(Dataset == j).iloc[6]] 
+    #Add to dataframe
+    DS1W = pd.concat([DS1W,Dataset[r]], axis = 1) 
+#Top metric    
+y = max(z1) 
+#Column ID of top metric
+k = Dataset.columns[(Dataset == y).iloc[6]]
+#Column ID of top metric - float
+kfloat = float(k[0]) 
+#End timer
 End = t.time()
+#Timer stats
 print(End-Start, 'seconds later')
-print(Dataset[k]) #Attractive parameter set
+#Top param set
+print(Dataset[k]) 
 
-#Now lets run the model and see what the attractive parameter set looks like.
-  
+#Read in params
+#Position sizing  
 Asset1['Position'] = (Dataset[kfloat][0])
+#Alternative position sizing
 Asset1['Position'] = np.where(Asset3['Contango'].shift(1) > Dataset[kfloat][4],
                                     Dataset[kfloat][2],Dataset[kfloat][0])
+#Apply position to returns
 Asset1['Pass'] = (Asset1['LogRet'] * Asset1['Position'])
-
+#Position sizing
 Asset2['Position'] = (Dataset[kfloat][1])
+#Alternative position sizing
 Asset2['Position'] = np.where(Asset3['Contango'].shift(1) > Dataset[kfloat][4],
             Dataset[kfloat][3],Dataset[kfloat][1])
+#Apply position to returns
 Asset2['Pass'] = (Asset2['LogRet'] * Asset2['Position'])
 
+#Pass individual return streams to portfolio
 Portfolio['Asset1Pass'] = Asset1['Pass'] * (-1)
 Portfolio['Asset2Pass'] = Asset2['Pass'] #* (-1)
-
+#Cumulative portfolio returns
 Portfolio['TotalRet'] = Portfolio['Asset1Pass'] + Portfolio['Asset2Pass'] 
+
+#Graphical display
 Portfolio['TotalRet'][:].cumsum().apply(np.exp).plot(grid=True, figsize=(8,5))
+
+#Performance metrics
 dailyreturn = Portfolio['TotalRet'].mean()
 dailyvol = Portfolio['TotalRet'].std()
 sharpe =(dailyreturn/dailyvol)
-Portfolio['Multiplier'] = Portfolio['TotalRet'].cumsum().apply(np.exp)
-drawdown2 =  1 - Portfolio['Multiplier'].div(Portfolio['Multiplier'].cummax())
 
+#Returns on $1
+Portfolio['Multiplier'] = Portfolio['TotalRet'].cumsum().apply(np.exp)
+#Incorrectly calculated drawdown
+drawdown2 =  1 - Portfolio['Multiplier'].div(Portfolio['Multiplier'].cummax())
+#Display results
 print('Max drawdown is about ' + str(round((max(drawdown2)*100),2)) +'%')
